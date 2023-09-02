14787739d580aab59c17b2bb36066dd69248b448
UnsignedByte <edlam1889@gmail.com>
2023-09-01 18:51:29 -0700

    add template duplication script

--- template/template.v
+++ template/template.v
@@ -5,16 +5,22 @@
 `ifndef TEMPLATE_V
 `define TEMPLATE_V
 
-module template
-(
-  input  logic clk;
-  input  logic q;
-  output logic d;
-);
+module Template 
+# (
+  parameter width = 32,
+) (
+  input  logic clk,
+  input  logic reset,
 
-always_ff @( posedge clk )
-  q <= d;
+	output logic recv_rdy,
+	input  logic recv_val,
+  input  logic [width-1:0] recv_msg,
+
+	input  logic send_rdy,
+	output logic send_val,
+  output logic [width-1:0] send_msg,
+);
 
 endmodule
 
-`endif
+`endif /* TEMPLATE_V */
