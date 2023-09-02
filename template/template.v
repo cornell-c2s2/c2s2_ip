205711fdf341ce68ec8041d15e639a4e3c24a12c
UnsignedByte <edlam1889@gmail.com>
2023-09-02 08:07:22 -0700

    add svlint support

--- template/template.v
+++ template/template.v
@@ -1,26 +1,27 @@
 //================================================
 // template.v
 //================================================
-
+`default_nettype none
 `ifndef TEMPLATE_V
 `define TEMPLATE_V
 
-module Template 
-# (
-  parameter width = 32,
+module Template #(
+  parameter int width = 32
 ) (
   input  logic clk,
   input  logic reset,
 
-	output logic recv_rdy,
-	input  logic recv_val,
-  input  logic [width-1:0] recv_msg,
+  output logic recv_rdy,
+  input  logic recv_val,
+  input  logic [width - 1:0] recv_msg,
 
-	input  logic send_rdy,
-	output logic send_val,
-  output logic [width-1:0] send_msg,
+  input  logic send_rdy,
+  output logic send_val,
+  output logic [width - 1:0] send_msg
 );
-
+  always_comb begin
+    send_msg = recv_msg;
+  end
 endmodule
 
-`endif /* TEMPLATE_V */
+`endif
