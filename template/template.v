054d77ecc70b2b2414b4b5016434c45a6be14e0a
UnsignedByte <edlam1889@gmail.com>
2023-09-03 17:17:17 -0700

    fix template

--- template/template.v
+++ template/template.v
@@ -1,10 +1,10 @@
 //================================================
 // template.v
 //================================================
-`default_nettype none
 `ifndef TEMPLATE_V
 `define TEMPLATE_V
 
+<<<<<<< HEAD:template/template.v
 module Template #(
   parameter int width = 32
 ) (
@@ -14,12 +14,27 @@ module Template #(
   output logic recv_rdy,
   input  logic recv_val,
   input  logic [width - 1:0] recv_msg,
+=======
+module template #(
+  parameter width = 32
+) (
+  input clk,
+  input logic reset,
+
+  output logic recv_rdy,
+  input logic recv_val,
+  input logic [Width-1:0] recv_msg,
+>>>>>>> 1ec3219... updates imports:template/Template.v
 
   input  logic send_rdy,
   output logic send_val,
+<<<<<<< HEAD:template/template.v
   output logic [width - 1:0] send_msg
+=======
+  output logic [Width-1:0] send_msg
+>>>>>>> 1ec3219... updates imports:template/Template.v
 );
-  always_comb begin
+  always @(*) begin
     send_msg = recv_msg;
   end
 endmodule
