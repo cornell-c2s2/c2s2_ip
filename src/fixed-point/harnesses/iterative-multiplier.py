1de47ead7b912484295526edef1775053572eba4
UnsignedByte <edlam1889@gmail.com>
2023-09-03 18:36:12 -0700

    add tests

--- src/fixed-point/harnesses/iterative-multiplier.py
+++ src/fixed-point/harnesses/iterative-multiplier.py
@@ -2,21 +2,16 @@ from pymtl3 import *
 from pymtl3.stdlib import stream
 from pymtl3.passes.backends.verilog import *
 
-# Pymtl3 harness for the `Template` module.
+# Pymtl3 harness for the `FPIterativeMultiplier` module.
 class FPIterativeMultiplier( VerilogPlaceholder, Component ):
   # Constructor
 
   def construct( s, n=32, d=16 ):
     # Interface
-    s.a = InPort()
-    s.b = InPort()
-    s.sum = OutPort()
-    s.cout = OutPort()
-
-    s.recv = stream.ifcs.RecvIfcRTL( mk_bits(n) )
+    s.recv = stream.ifcs.RecvIfcRTL( mk_bits(2*n) )
     s.send = stream.ifcs.SendIfcRTL( mk_bits(n) )
 
     # Name of the top level module to be imported
-    s.set_metadata( VerilogPlaceholderPass.top_module, 'FPIterativeMultiplier' )
+    s.set_metadata( VerilogPlaceholderPass.top_module, 'HarnessPFIM' )
     # Source file path
-    s.set_metadata( VerilogPlaceholderPass.src_file, 'fixed-point/iterative-multiplier.v' )
+    s.set_metadata( VerilogPlaceholderPass.src_file, 'cmn/fixed-point/harnesses/iterative-multiplier.v' )
