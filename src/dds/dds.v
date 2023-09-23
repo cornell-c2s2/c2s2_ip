fe3d004bb905a4b37e9f50dd001a5ec2de2e73de
ss2723 <ss2723@cornell.edu>
2023-09-23 11:03:44 -0400

    Initial IP: dds

---
+++ src/dds/dds.v
@@ -0,0 +1,27 @@
+//================================================
+// dds.v
+//================================================
+`default_nettype none
+`ifndef DDS_V
+`define DDS_V
+
+module Dds #(
+  parameter int Width = 32
+) (
+  input logic clk,
+  input logic reset,
+
+  output logic recv_rdy,
+  input logic recv_val,
+  input logic [Width - 1:0] recv_msg,
+
+  input logic send_rdy,
+  output logic send_val,
+  output logic [Width - 1:0] send_msg
+);
+  always_comb begin
+    send_msg = recv_msg;
+  end
+endmodule
+
+`endif
