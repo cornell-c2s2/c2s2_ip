4339f836d24e4da9a5a8b2e183a21bcc3826fb2c
UnsignedByte <edlam1889@gmail.com>
2023-09-23 15:59:22 -0700

    fix style

--- src/fixed_point/iterative/butterfly.v
+++ src/fixed_point/iterative/butterfly.v
@@ -18,67 +18,80 @@
 `define PROJECT_BUTTERFLY_V
 `include "src/fixed_point/iterative/complex_multiplier.v"
 `include "src/cmn/regs.v"
-module FixedPointIterativeButterfly
-#(
-	parameter n = 32,
-	parameter d = 16,
-	parameter mult = 0
-	// Optimization parameter to save area:
-	// 0 if we include the multiplier
-	// 1 if omega = 1
-	// 2 if omega = -1
-	// 3 if omega = i (j)
-	// 3 if omega = -i (-j)
-) (clk, reset, recv_val, recv_rdy, send_val, send_rdy, ar, ac, br, bc, wr, wc, cr, cc, dr, dc);
-	/* performs the butterfly operation, equivalent to doing
-		| 1  w |   | a |   | c |
-		| 1 -w | * | b | = | d |
-	*/
+module FixedPointIterativeButterfly #(
+  parameter int n = 32,
+  parameter int d = 16,
+  parameter bit mult = 0
+  // Optimization parameter to save area:
+  // 0 if we include the multiplier
+  // 1 if omega = 1
+  // 2 if omega = -1
+  // 3 if omega = i (j)
+  // 3 if omega = -i (-j)
+) (
+  input logic clk,
+  input logic reset,
 
-	input logic clk, reset;
-	input logic recv_val, send_rdy;
-	input logic [n-1:0] ar, ac, br, bc, wr, wc;
-	output logic send_val, recv_rdy;
-	output logic [n-1:0] cr, cc, dr, dc;
+  input logic recv_val,
+  output logic recv_rdy,
+  output logic send_val,
+  input logic send_rdy,
 
-	logic [n-1:0] ar_imm, ac_imm;
+  input logic ar,
+  input logic ac,
+  input logic br,
+  input logic bc,
+  input logic wr,
+  input logic wc,
+  
+  output logic cr,
+  output logic cc,
+  output logic dr,
+  output logic dc
+);
+  /* performs the butterfly operation, equivalent to doing
+  | 1  w |   | a |   | c |
+  | 1 -w | * | b | = | d |
+  */
 
-	logic [n-1:0] tr, tc;
+  logic [n-1:0] ar_imm, ac_imm;
 
-	FixedPointIterativeComplexMultiplier #(.n(n), .d(d)) mul ( // ar * br
-        .clk(clk),
-        .reset(reset),
-        .ar(br),
-        .ac(bc),
-        .br(wr),
-        .bc(wc),
-        .cr(tr),
-        .cc(tc),
-        .recv_val(recv_val),
-        .recv_rdy(recv_rdy),
-        .send_val(send_val),
-        .send_rdy(send_rdy)
-            );
-	
-	cmn_EnResetReg #(n) ac_reg (
-        .clk(clk),
-        .en(recv_rdy),
-        .d(ac),
-        .q(ac_imm),
-        .reset(reset)
-    );
-	cmn_EnResetReg #(n) ar_reg (
-        .clk(clk),
-        .en(recv_rdy),
-        .d(ar),
-        .q(ar_imm),
-        .reset(reset)
-    );
+  logic [n-1:0] tr, tc;
+
+  FixedPointIterativeComplexMultiplier #(.n(n), .d(d)) mul ( // ar * br
+    .clk(clk),
+    .reset(reset),
+    .ar(br),
+    .ac(bc),
+    .br(wr),
+    .bc(wc),
+    .cr(tr),
+    .cc(tc),
+    .recv_val(recv_val),
+    .recv_rdy(recv_rdy),
+    .send_val(send_val),
+    .send_rdy(send_rdy)
+  );
+  
+  cmn_EnResetReg #(n) ac_reg (
+    .clk(clk),
+    .en(recv_rdy),
+    .d(ac),
+    .q(ac_imm),
+    .reset(reset)
+  );
+  cmn_EnResetReg #(n) ar_reg (
+    .clk(clk),
+    .en(recv_rdy),
+    .d(ar),
+    .q(ar_imm),
+    .reset(reset)
+  );
 
 
-	assign cr = ar_imm + tr;
-	assign cc = ac_imm + tc;
-	assign dr = ar_imm - tr;
-	assign dc = ac_imm - tc;
+  assign cr = ar_imm + tr;
+  assign cc = ac_imm + tc;
+  assign dr = ar_imm - tr;
+  assign dc = ac_imm - tc;
 endmodule
 `endif
