b8d2865bf74300c209a7965b7844d4dfe4155612
fl327 <fl327@cornell.edu>
2023-09-30 12:17:29 -0400

    fix more stuff

--- src/fixed_point/combinational/FpCombMultVRTL.v
+++ src/fixed_point/combinational/FpCombMultVRTL.v
@@ -2,31 +2,30 @@
 `ifndef FIXED_POINT_COMB_MULTIPLIER
 `define FIXED_POINT_COMB_MULTIPLIER
 
-module FpCombMultVRTL
-# (
-    parameter int n = 32, // bit width
-    parameter int d = 16, // number of decimal bits
-    parameter bit sign = 1 // 1 if signed, 0 otherwise.
-)(
-    input  logic [n-1:0] a, 
+module FpCombMultVRTL #(
+    parameter int n = 32,  // bit width
+    parameter int d = 16,  // number of decimal bits
+    parameter bit sign = 1  // 1 if signed, 0 otherwise.
+) (
+    input  logic [n-1:0] a,
     input  logic [n-1:0] b,
     output logic [n-1:0] c
 );
 
-logic [2*n-1:0] prod;
-logic [2*n-1:0] a_ext, b_ext;
+  logic [2*n-1:0] prod;
+  logic [2*n-1:0] a_ext, b_ext;
 
-generate 
-    if ( sign ) begin
-        assign a_ext = {{n{a[n-1]}}, a};
-        assign b_ext = {{n{b[n-1]}}, b};
-        assign prod = (a_ext * b_ext) >>> d;
+  generate
+    if (sign) begin
+      assign a_ext = {{n{a[n-1]}}, a};
+      assign b_ext = {{n{b[n-1]}}, b};
+      assign prod  = (a_ext * b_ext) >>> d;
     end else begin
-        assign prod = (a * b) >> d;
+      assign prod = (a * b) >> d;
     end
-endgenerate
+  endgenerate
 
-assign c = prod[n-1:0];
+  assign c = prod[n-1:0];
 
 endmodule
 
