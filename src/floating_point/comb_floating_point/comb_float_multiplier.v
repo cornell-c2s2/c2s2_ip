434e69b68262f4683e1120ca55e7b6cee1d93aab
mll264 <mll264@cornell.edu>
2023-10-21 11:37:24 -0400

    more work on combinational multiplier

--- src/floating_point/comb_floating_point/comb_float_multiplier.v
+++ src/floating_point/comb_floating_point/comb_float_multiplier.v
@@ -10,15 +10,41 @@
 
 module comb_float_multiplier #(
 ) (
-  input  logic [15:0] in0,
-  input  logic [15:0] in1,
-  output logic [15:0] out
+  input  logic [31:0] in0,
+  input  logic [31:0] in1,
+  output logic [31:0] out
 );
 
-  logic s0, s1;
-  logic [3:0] e0, e1;
-  logic [10:0] m0, m1;
+  logic s0, s1, sout;  //sign
+  logic [7:0] e0, e1, eout;  // exponent
+  logic [22:0] m0, m1, mout;  // mantissa
 
+  logic [7:0] bias = 8'd127;  // bias for 32 bit floating pt
+
+  assign s0   = in0[31];
+  assign e0   = in0[30:23];
+  assign m0   = in0[22:0];
+
+  assign s1   = in1[31];
+  assign e1   = in1[30:23];
+  assign m1   = in1[22:0];
+
+  // sign 
+  assign sout = s0 ^ s1;
+
+  // exponent 
+  assign eout = e0 + e1 - bias;
+
+  // mantissa
+  FixedPointCombMultiplier #(
+    .n   (24),
+    .d   (23),
+    .sign(0)
+  ) mantissa_mult (
+    .a({1, m0}),
+    .b({1, m1}),
+    .c({1, mout})
+  );
 
 endmodule
 
