ef6ebe492f4beb687ddcd3e137c56692b80d9df8
ayc62 <ayc62@cornell.edu>
2023-10-22 16:32:33 -0400

    Initial IP: wishbone

---
+++ src/wishbone/harnesses/wishbone.py
@@ -0,0 +1,24 @@
+from pymtl3 import *
+from pymtl3.stdlib import stream
+from pymtl3.passes.backends.verilog import *
+from os import path
+
+
+# Pymtl3 harness for the `Wishbone` module.
+class Wishbone(VerilogPlaceholder, Component):
+    # Constructor
+
+    def construct(s):
+        # Interface
+
+        s.recv = stream.ifcs.RecvIfcRTL(mk_bits(n))
+        s.send = stream.ifcs.SendIfcRTL(mk_bits(n))
+
+        # Name of the top level module to be imported
+        s.set_metadata(VerilogPlaceholderPass.top_module, "Wishbone")
+        # Source file path
+        # The ../ is necessary here because pytest is run from the build directory
+        s.set_metadata(
+            VerilogPlaceholderPass.src_file,
+            path.join(path.dirname(__file__), "../wishbone.v"),
+        )
---
+++ src/wishbone/wishbone.v
@@ -0,0 +1,27 @@
+//================================================
+// wishbone.v
+//================================================
+`default_nettype none
+`ifndef WISHBONE_V
+`define WISHBONE_V
+
+module Wishbone #(
+  parameter int Width = 32
+) (
+  input logic clk,
+  input logic reset,
+
+  output logic recv_rdy,
+  input logic recv_val,
+  input logic [Width - 1:0] recv_msg,
+
+  input logic send_rdy,
+  output logic send_val,
+  output logic [Width - 1:0] send_msg
+);
+  always_comb begin
+    send_msg = recv_msg;
+  end
+endmodule
+
+`endif
