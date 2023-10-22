c307ef04468e49aa1155049b1a5b5d7fb45bf280
mll264 <mll264@cornell.edu>
2023-10-22 18:45:48 -0400

    added comb floating point divider file

---
+++ src/floating_point/comb_floating_point/comb_float_divider.v
@@ -0,0 +1,21 @@
+//================================================
+// comb_float_divider.v
+// 
+// Combinational floating point multiplier
+// Author: Mattie Lee (mll264)
+// Additional credits: Barry Lyu (fl327), Xilai Dai (xd44)
+//================================================
+
+`ifndef COMB_FLOAT_DIVIDER_V
+`define COMB_FLOAT_DIVIDER_V
+
+module comb_float_divider #(
+) (
+  input  logic [31:0] in0,
+  input  logic [31:0] in1,
+  output logic [31:0] out
+);
+
+endmodule
+
+`endif
