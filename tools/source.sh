e4333ffb35cb07687d18987a3bcc13fcd8817573
el595 <el595@cornell.edu>
2023-10-28 17:14:35 -0400

    source setup script by default

--- tools/source.sh
+++ tools/source.sh
@@ -1,4 +1,9 @@
 source ~/.bashrc
 source .venv/bin/activate
 
+# Run setup-c2s2.sh if we are on the ecelinux machine
+if [ -f /classes/c2s2/setup-c2s2.sh ]; then
+  source setup-c2s2.sh
+fi
+
 echo -e "\033[0;32mC2S2 Workspace Initialized.\033[0m"
