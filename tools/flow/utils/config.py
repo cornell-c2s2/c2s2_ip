03ae171b2121a1860544a435d76760259e052701
Edmund Lam <el595@cornell.edu>
2023-11-19 13:43:21 -0800

    Integrate OpenLANE flow into repository (#79)
    * fix caravel dockerfile
    
    * add caravel dockerfile
    
    * get started with automated flow
    
    * fix caravel
    
    * change locations
    
    * make openlane non-read-only
    
    * further dockerfile changes
    
    * add new docker container setup
    
    * start on flow tools
    
    * remove unused import
    
    * fmt
    
    * remove caravel from makefile
    
    * add more flow stuff
    
    * restructure
    
    * get started on synth pass
    
    * add synthesis script
    
    * working synthesis
    
    * add synthesis script
    
    * add caravel lyp
    
    * update openlane root
    
    * fix bugs

---
+++ tools/flow/utils/config.py
@@ -0,0 +1,33 @@
+from os import path
+import json
+
+CONFIG_FILE = path.join(path.dirname(__file__), "config.json")
+
+
+def get_config() -> dict:
+    """
+    Get the configuration fil
+    """
+    if not path.exists(CONFIG_FILE):
+        with open(CONFIG_FILE, "w") as config_file:
+            json.dump({}, config_file)
+
+    with open(CONFIG_FILE, "r") as config_file:
+        config = json.load(config_file)
+    return config
+
+
+def get_user() -> str:
+    """
+    Get the username from the config file
+    """
+    config = get_config()
+    user = config.get("user", None)
+    if user is None:
+        print("No user found in the config file. Please type in your cornell NetID:")
+        config["user"] = input("> ")
+        with open(CONFIG_FILE, "w") as config_file:
+            json.dump(config, config_file)
+        user = config["user"]
+
+    return user
