46b84109f6af66c02c6129510842f7df2bec33b7
tic3 <tic3@cornell.edu>
2024-03-04 23:13:17 -0500

    Initial IP: classifier

---
+++ src/classifier/classifier.v
@@ -0,0 +1,27 @@
+//================================================
+// classifier.v
+//================================================
+`default_nettype none
+`ifndef CLASSIFIER_V
+`define CLASSIFIER_V
+
+module classifier_Classifier #(
+  parameter int Width = 32
+) (
+  input logic clk,
+  input logic reset,
+
+  output logic recv_rdy,
+  input logic recv_val,
+  input logic [Width - 1:0] recv_msg,
+
+  input logic send_rdy,
+  output logic send_val,
+  output logic [Width - 1:0] send_msg
+);
+  always_comb begin
+    send_msg = recv_msg;
+  end
+endmodule
+
+`endif
