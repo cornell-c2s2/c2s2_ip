a896dcff6bf8d8e53171989be823316f259b9597
tic3 <tic3@cornell.edu>
2024-03-14 00:03:16 -0400

    Wrote some tests and they pass

--- src/classifier/tests/classifier.py
+++ src/classifier/tests/classifier.py
@@ -19,6 +19,9 @@ import wave
 def make_fixed (n, d, v):
     max_fraction_value = 2**d
     scaled_number = round(v * max_fraction_value)
+    print(v)
+    print(v * max_fraction_value)
+    print(scaled_number)
     return scaled_number
 
 def make_arr_fixed (n, d, a):
@@ -42,6 +45,18 @@ def fixed_bits(f: Fixed) -> Bits:
 
     return mk_bits(len(f))(value)
 
+def sine_wave_gen():
+    t = np.linspace(0, 1, 16, endpoint=False)  
+    freq = 2500  
+    A = 100 
+    sine_wave = A * np.sin(2 * np.pi * freq * t)
+    return sine_wave
+
+def get_fft_mag (arr):
+    frq_arr = np.fft.fft(arr)
+    mag = np.abs(frq_arr)
+    return mag
+
 # -------------------------------------------------------------------------
 # TestHarness
 # -------------------------------------------------------------------------
@@ -61,34 +76,101 @@ class TestHarness(Component):
     def done(s):
         return s.src.done() and s.sink.done()
 
-def false_test():
-    audio_array = np.array([0,0.5,1,0.5,0,0.5,1,0.5,0,0.5,1,0.5,0,0.5,1,0.5,0,0.5,1,0.5,0,0.5,1,0.5,0,0.5,1,0.5,0,0.5,1,0.5,0,0.5,1,0.5,0,0.5,1,0.5,0,0.5,1,0.5,0,0.5,1,0.5,0,0.5,1,0.5,0,0.5,1,0.5,0,0.5,1,0.5,0,0.5,1,0.5])
-    audio_array = audio_array - 0.5
-    sample_rate = 50000
-    frq_arr = np.fft.fft(audio_array[:16])
-    real_part = frq_arr.real
-    return [real_part, 0]
+# -------------------------------------------------------------------------
+# Tests
+# -------------------------------------------------------------------------
+def sine_wave():
+    arr = sine_wave_gen()
+    frq_arr = np.fft.fft(arr)
+    mag = np.abs(frq_arr)
+    return [mag, 0]
 
-def true_test():
-    file_path = '/home/tic3/c2s2_ip/src/classifier/audio_files/ABS_MCBY_-YWX_MixPre-672.WAV'
-    audio_array, sample_rate, num_channels = read_wav_file(file_path)
-    audio_array_0 = audio_array[:64]
-    #audio_array_1 = audio_array[64:128]
-    audio_array_1 = audio_array[64:72]
-    audio_array_2 = audio_array[128:192]
-    audio_array_3 = audio_array[192:256]
-    frq_arr = np.fft.fft(audio_array_1)
-    real_part = frq_arr.real
-    return [real_part, 1]
+def below_freq_mag (): # CUTOFF_FREQ = 8250 Hz, CUTOFF_MAG = 10
+    return [get_fft_mag([0, 0.27, 0.49, 0.65, 0.7, 0.65, 0.49, 0.27, 0, -0.27, -0.49, -0.65, -0.7, -0.65, -0.49, -0.27]), 0,
+            get_fft_mag([0, 0.42, 0.6, 0.42, 0, -0.42, -0.6, -0.42, 0, 0.42, 0.6, 0.42, 0, -0.42, -0.6, -0.42]), 0,
+            get_fft_mag([0, 0.4, 0.69, 0.8, 0.69, 0.4, 0, -0.4, -0.69, -0.8, -0.69, -0.4, 0, 0.4, 0.69, 0.8]), 0]
+
+def above_freq_mag (): # CUTOFF_FREQ = 10000 Hz, CUTOFF_MAG = 20
+    return [get_fft_mag([20, -20, 20, -20, 20, -20, 20, -20, 20, -20, 20, -20, 20, -20, 20, -20]), 1,
+            get_fft_mag([ 0.00000000e+00,  2.44929360e-15, -4.89858720e-15,  7.34788079e-15,
+                         -9.79717439e-15,  1.22464680e-14, -1.46957616e-14,  1.71450552e-14,
+                         -1.95943488e-14,  2.20436424e-14, -2.44929360e-14,  9.79965032e-14,
+                         -2.93915232e-14, -3.92134568e-14, -3.42901104e-14,  1.07793678e-13]), 1,
+            get_fft_mag([ -5.01839525,  18.02857226,   9.27975767,   3.94633937, -13.75925438,
+                         -13.76021919, -17.67665551,  14.64704583,   4.04460047,   8.32290311,
+                         -19.17662023,  18.79639409,  13.29770563, -11.50643557, -12.72700131,
+                         -12.66381961]), 1]
+
+def above_freq_below_mag (): # CUTOFF_FREQ = 10000 Hz, CUTOFF_MAG = 20
+    return [get_fft_mag([2.0,
+                        2.914213562373095,
+                        3.0616169978683826e-16,
+                        -2.914213562373095,
+                        -2.0000000000000004,
+                        0.08578643762690463,
+                        1.8369701987210302e-16,
+                        -0.0857864376269053,
+                        1.9999999999999993,
+                        2.9142135623730954,
+                        1.5308084989341917e-15,
+                        -2.914213562373093,
+                        -2.000000000000001,
+                        0.08578643762690574,
+                        4.286263797015736e-16,
+                        -0.08578643762690685]), 0,
+            get_fft_mag([ 0.0,
+                            1.8477590650225735,
+                            -1.414213562373095,
+                            -0.7653668647301808,
+                            2.0,
+                            -0.7653668647301797,
+                            -1.414213562373097,
+                            1.8477590650225728,
+                            1.2246467991473533e-15,
+                            -1.8477590650225737,
+                            1.4142135623730951,
+                            0.7653668647301787,
+                            -2.0,
+                            0.7653668647301752,
+                            1.4142135623730978,
+                            -1.8477590650225724]), 0,
+            get_fft_mag([ -0.6400509785799624,
+                            -4.740737681721087,
+                            0.49662477878709144,
+                            -0.6467760738172315,
+                            -0.7963219791251097,
+                            -1.6966517899612588,
+                            -2.953513659621575,
+                            1.192709663506637,
+                            -2.0034532632547686,
+                            -2.3317272489713337,
+                            1.2113383276929488,
+                            0.2914209427703902,
+                            -3.6542005465506646,
+                            0.13578121265746468,
+                            -3.155601343530847,
+                            2.8533514781667346]), 0]
+
+def one_true (): # CUTOFF_FREQ = 10000 Hz, CUTOFF_MAG = 40
+    return [get_fft_mag([40, -40, 40, -40, 40, -40, 40, -40, 40, -40, 40, -40, 40, -40, 40, -40]), 1,
+            get_fft_mag([40 * np.sin(2 * np.pi * 4 * t / 16) for t in np.arange(16) ]), 1,
+            get_fft_mag([ 20 * np.sin(2 * np.pi * 3 * t / 16) + 20 * np.cos(2 * np.pi * 5 * t / 16) for t in np.arange(16) ]), 1]
 
 
 test_case_table = mk_test_case_table(
     [
         (
-                        "msgs        src_delay sink_delay BIT_WIDTH DECIMAL_PT N_SAMPLES CUTOFF_FREQ CUTOFF_MAG SAMPLING_FREQUENCY slow"
+                                  "msgs                  src_delay sink_delay BIT_WIDTH DECIMAL_PT N_SAMPLES CUTOFF_FREQ CUTOFF_MAG SAMPLING_FREQUENCY slow"
         ),
-        ["false_test",   false_test, 4,        4,         32,       16,        16,       65536000,   1310720,   50000,         False],
-        #["true_test",    true_test,  4,        4,         32,       16,        8,       65536000,   1310720,   96000,        False],
+        ["below_freq_mag",         below_freq_mag,       4,        4,         32,       16,        16,       540672000,   655360,     44000,         False],
+        ["above_freq_mag",         above_freq_mag,       4,        4,         32,       16,        16,       655360000,   1310720,    44000,         False],
+        ["above_freq_below_mag",   above_freq_below_mag, 4,        4,         32,       16,        16,       655360000,   1310720,    44000,         False],
+        ["one_true",               one_true,             4,        4,         32,       16,        16,       655360000,   2621440,    44000,         False],
+        #["b_freq_l_amp_sin_1",   b_freq_l_amp_sin_1, 4,        4,         32,       16,        16,       32768000,   32768,     44000,         False],
+        #["b_freq_l_amp_sin_2",   b_freq_l_amp_sin_2, 4,        4,         32,       16,        16,       32768000,   32768,     44000,         False],
+        # ["sine_wave",   sine_wave, 4,        4,         32,       16,        16,       32768000,   32768,     44000,         False],
+        # ["false_test",   false_test, 4,        4,         32,       16,        16,       65536000,   1310720,   50000,         False],
+        # ["true_test",    true_test,                  4,        4,         32,       10,        16,       65536000,   1310720,   96000,        False],
     ]
 )
 
@@ -105,13 +187,10 @@ def test(test_params, cmdline_opts):
 
     # inputs = [fixed_bits(x) for sample in inputs for x in sample]
     # outputs = [x for x in outputs]
-
     msgs = test_params.msgs()
     print(msgs)
     msgs = [make_arr_fixed(test_params.BIT_WIDTH, test_params.DECIMAL_PT, x) if i%2 != 0 else x for i, x in enumerate(msgs, start=1)]
-    print(msgs)
     msgs = [mk_packed(test_params.BIT_WIDTH)(*x) if i%2 != 0 else x for i, x in enumerate(msgs, start=1)]
-    print(msgs)
 
     th.set_param(
         "top.src.construct",
