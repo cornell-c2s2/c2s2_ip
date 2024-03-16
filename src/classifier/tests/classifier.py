f3f68a7458f391f5546d6632f21599cb26521127
tic3 <tic3@cornell.edu>
2024-03-16 08:10:22 -0400

    Addes more tests

--- src/classifier/tests/classifier.py
+++ src/classifier/tests/classifier.py
@@ -19,9 +19,6 @@ import wave
 def make_fixed (n, d, v):
     max_fraction_value = 2**d
     scaled_number = round(v * max_fraction_value)
-    print(v)
-    print(v * max_fraction_value)
-    print(scaled_number)
     return scaled_number
 
 def make_arr_fixed (n, d, a):
@@ -29,15 +26,6 @@ def make_arr_fixed (n, d, a):
 
 def pack_msg (n, arr):
     return mk_packed(n)(arr)
-
-def read_wav_file(file_path):
-    with wave.open(file_path, 'rb') as wav_file:
-        audio_data = wav_file.readframes(-1)
-        audio_array = np.frombuffer(audio_data, dtype=np.int16)
-        sample_rate = wav_file.getframerate()
-        num_channels = wav_file.getnchannels()
-
-    return audio_array, sample_rate, num_channels
 
 # Cast a Fixed object to a Bits object
 def fixed_bits(f: Fixed) -> Bits:
@@ -52,10 +40,37 @@ def sine_wave_gen():
     sine_wave = A * np.sin(2 * np.pi * freq * t)
     return sine_wave
 
-def get_fft_mag (arr):
-    frq_arr = np.fft.fft(arr)
-    mag = np.abs(frq_arr)
-    return mag
+def get_fft_real (arr):
+    fft_arr = np.fft.fft(arr)
+    real = np.real(fft_arr)
+    return real
+
+def classifier(fft_arr, cutoff_frq, cutoff_mag):
+    fft_mags = np.abs(np.real(fft_arr))
+    frq_arr = np.fft.fftfreq(len(fft_arr), 1/44000)
+    fft_hi_pass = fft_mags[np.nonzero(frq_arr>cutoff_frq)]
+    max_amplitude = np.max(fft_hi_pass)
+    
+    if max_amplitude > cutoff_mag:
+        return 1
+    else: 
+        return 0
+
+def generate_sine_wave_below_mag(frequency, sample_freq, cutoff_mag):
+    length = 16
+    t = np.arange(0, length * (1/sample_freq), (1/sample_freq))
+    amplitude = cutoff_mag / 100
+    sine_wave = amplitude * np.sin(2 * np.pi * frequency * t)
+    
+    fft_output = np.fft.fft(sine_wave)
+    real_part = np.abs(fft_output.real)
+
+    if np.any(real_part > cutoff_mag):
+        print("Adjusting amplitude to meet requirements...")
+        sine_wave *= (cutoff_mag / np.max(real_part))
+    
+    return sine_wave
+
 
 # -------------------------------------------------------------------------
 # TestHarness
@@ -79,82 +94,151 @@ class TestHarness(Component):
 # -------------------------------------------------------------------------
 # Tests
 # -------------------------------------------------------------------------
-def sine_wave():
-    arr = sine_wave_gen()
-    frq_arr = np.fft.fft(arr)
-    mag = np.abs(frq_arr)
-    return [mag, 0]
-
 def below_freq_mag (): # CUTOFF_FREQ = 8250 Hz, CUTOFF_MAG = 10
-    return [get_fft_mag([0, 0.27, 0.49, 0.65, 0.7, 0.65, 0.49, 0.27, 0, -0.27, -0.49, -0.65, -0.7, -0.65, -0.49, -0.27]), 0,
-            get_fft_mag([0, 0.42, 0.6, 0.42, 0, -0.42, -0.6, -0.42, 0, 0.42, 0.6, 0.42, 0, -0.42, -0.6, -0.42]), 0,
-            get_fft_mag([0, 0.4, 0.69, 0.8, 0.69, 0.4, 0, -0.4, -0.69, -0.8, -0.69, -0.4, 0, 0.4, 0.69, 0.8]), 0]
+    input_1 = [0, 0.27, 0.49, 0.65, 0.7, 0.65, 0.49, 0.27, 0, -0.27, -0.49, -0.65, -0.7, -0.65, -0.49, -0.27]
+    input_2 = [0, 0.42, 0.6, 0.42, 0, -0.42, -0.6, -0.42, 0, 0.42, 0.6, 0.42, 0, -0.42, -0.6, -0.42]
+    input_3 = [0, 0.4, 0.69, 0.8, 0.69, 0.4, 0, -0.4, -0.69, -0.8, -0.69, -0.4, 0, 0.4, 0.69, 0.8]
+    return [get_fft_real(input_1), classifier(np.fft.fft(input_1), 8250, 10),
+            get_fft_real(input_2), classifier(np.fft.fft(input_2), 8250, 10),
+            get_fft_real(input_3), classifier(np.fft.fft(input_3), 8250, 10)]
 
 def above_freq_mag (): # CUTOFF_FREQ = 10000 Hz, CUTOFF_MAG = 20
-    return [get_fft_mag([20, -20, 20, -20, 20, -20, 20, -20, 20, -20, 20, -20, 20, -20, 20, -20]), 1,
-            get_fft_mag([ 0.00000000e+00,  2.44929360e-15, -4.89858720e-15,  7.34788079e-15,
-                         -9.79717439e-15,  1.22464680e-14, -1.46957616e-14,  1.71450552e-14,
-                         -1.95943488e-14,  2.20436424e-14, -2.44929360e-14,  9.79965032e-14,
-                         -2.93915232e-14, -3.92134568e-14, -3.42901104e-14,  1.07793678e-13]), 1,
-            get_fft_mag([ -5.01839525,  18.02857226,   9.27975767,   3.94633937, -13.75925438,
-                         -13.76021919, -17.67665551,  14.64704583,   4.04460047,   8.32290311,
-                         -19.17662023,  18.79639409,  13.29770563, -11.50643557, -12.72700131,
-                         -12.66381961]), 1]
+    input_1 = [ 1.00000000e+01,  2.83276945e-15, -1.00000000e+01, -1.83697020e-15,
+          1.00000000e+01,  1.19434012e-14, -1.00000000e+01, -4.28626380e-15,
+          1.00000000e+01,  2.32744790e-14, -1.00000000e+01, -2.44991258e-14,
+          1.00000000e+01,  2.57237726e-14, -1.00000000e+01, -2.69484194e-14]
+    input_2 = [ 1.00000000e+01, -3.82683432e+00, -7.07106781e+00,  9.23879533e+00,
+          1.19434012e-14, -9.23879533e+00,  7.07106781e+00,  3.82683432e+00,
+         -1.00000000e+01,  3.82683432e+00,  7.07106781e+00, -9.23879533e+00,
+          8.57871740e-15,  9.23879533e+00, -7.07106781e+00, -3.82683432e+00]
+    input_3 = [ 1.00000000e+01, -7.07106781e+00, -1.83697020e-15,  7.07106781e+00,
+         -1.00000000e+01,  7.07106781e+00, -1.22526578e-14, -7.07106781e+00,
+          1.00000000e+01, -7.07106781e+00,  8.57871740e-15,  7.07106781e+00,
+         -1.00000000e+01,  7.07106781e+00, -4.90477700e-15, -7.07106781e+00]
+    return [get_fft_real(input_1), classifier(np.fft.fft(input_1), 10000, 20),
+            get_fft_real(input_2), classifier(np.fft.fft(input_2), 10000, 20),
+            get_fft_real(input_3), classifier(np.fft.fft(input_3), 10000, 20)]
 
 def above_freq_below_mag (): # CUTOFF_FREQ = 10000 Hz, CUTOFF_MAG = 20
-    return [get_fft_mag([2.0,
-                        2.914213562373095,
-                        3.0616169978683826e-16,
-                        -2.914213562373095,
-                        -2.0000000000000004,
-                        0.08578643762690463,
-                        1.8369701987210302e-16,
-                        -0.0857864376269053,
-                        1.9999999999999993,
-                        2.9142135623730954,
-                        1.5308084989341917e-15,
-                        -2.914213562373093,
-                        -2.000000000000001,
-                        0.08578643762690574,
-                        4.286263797015736e-16,
-                        -0.08578643762690685]), 0,
-            get_fft_mag([ 0.0,
-                            1.8477590650225735,
-                            -1.414213562373095,
-                            -0.7653668647301808,
-                            2.0,
-                            -0.7653668647301797,
-                            -1.414213562373097,
-                            1.8477590650225728,
-                            1.2246467991473533e-15,
-                            -1.8477590650225737,
-                            1.4142135623730951,
-                            0.7653668647301787,
-                            -2.0,
-                            0.7653668647301752,
-                            1.4142135623730978,
-                            -1.8477590650225724]), 0,
-            get_fft_mag([ -0.6400509785799624,
-                            -4.740737681721087,
-                            0.49662477878709144,
-                            -0.6467760738172315,
-                            -0.7963219791251097,
-                            -1.6966517899612588,
-                            -2.953513659621575,
-                            1.192709663506637,
-                            -2.0034532632547686,
-                            -2.3317272489713337,
-                            1.2113383276929488,
-                            0.2914209427703902,
-                            -3.6542005465506646,
-                            0.13578121265746468,
-                            -3.155601343530847,
-                            2.8533514781667346]), 0]
+    input_1 = [ 2.0,
+                2.914213562373095,
+                3.0616169978683826e-16,
+                -2.914213562373095,
+                -2.0000000000000004,
+                0.08578643762690463,
+                1.8369701987210302e-16,
+                -0.0857864376269053,
+                1.9999999999999993,
+                2.9142135623730954,
+                1.5308084989341917e-15,
+                -2.914213562373093,
+                -2.000000000000001,
+                0.08578643762690574,
+                4.286263797015736e-16,
+                -0.08578643762690685]
+    input_2 = [ 0.0,
+                1.8477590650225735,
+                -1.414213562373095,
+                -0.7653668647301808,
+                2.0,
+                -0.7653668647301797,
+                -1.414213562373097,
+                1.8477590650225728,
+                1.2246467991473533e-15,
+                -1.8477590650225737,
+                1.4142135623730951,
+                0.7653668647301787,
+                -2.0,
+                0.7653668647301752,
+                1.4142135623730978,
+                -1.8477590650225724]
+    input_3 = [ -0.6400509785799624,
+                -4.740737681721087,
+                0.49662477878709144,
+                -0.6467760738172315,
+                -0.7963219791251097,
+                -1.6966517899612588,
+                -2.953513659621575,
+                1.192709663506637,
+                -2.0034532632547686,
+                -2.3317272489713337,
+                1.2113383276929488,
+                0.2914209427703902,
+                -3.6542005465506646,
+                0.13578121265746468,
+                -3.155601343530847,
+                2.8533514781667346]
+    return [get_fft_real(input_1), classifier(np.fft.fft(input_1), 10000, 20),
+            get_fft_real(input_2), classifier(np.fft.fft(input_2), 10000, 20),
+            get_fft_real(input_3), classifier(np.fft.fft(input_3), 10000, 20)]
 
 def one_true (): # CUTOFF_FREQ = 10000 Hz, CUTOFF_MAG = 40
-    return [get_fft_mag([40, -40, 40, -40, 40, -40, 40, -40, 40, -40, 40, -40, 40, -40, 40, -40]), 1,
-            get_fft_mag([40 * np.sin(2 * np.pi * 4 * t / 16) for t in np.arange(16) ]), 1,
-            get_fft_mag([ 20 * np.sin(2 * np.pi * 3 * t / 16) + 20 * np.cos(2 * np.pi * 5 * t / 16) for t in np.arange(16) ]), 1]
+    input_1 = [40, -40, 40, -40, 40, -40, 40, -40, 40, -40, 40, -40, 40, -40, 40, -40]
+    input_2 = [40 * np.sin(2 * np.pi * 4 * t / 16) for t in np.arange(16) ]
+    input_3 = [ 20 * np.sin(2 * np.pi * 3 * t / 16) + 20 * np.cos(2 * np.pi * 5 * t / 16) for t in np.arange(16) ]
+    return [get_fft_real(input_1), classifier(np.fft.fft(input_1), 10000, 40),
+            get_fft_real(input_2), classifier(np.fft.fft(input_2), 10000, 40),
+            get_fft_real(input_3), classifier(np.fft.fft(input_3), 10000, 40)]
+
+def random_4_decimal (): # BIT_WIDTH = 32, DECIMAL_PT = 4
+    np.random.seed(42)
+    length = 16
+    random_1 = [random.uniform(0, 25000000) for _ in range(length)]
+    random_2 = [random.uniform(0, 25000000) for _ in range(length)]
+    random_3 = [random.uniform(0, 25000000) for _ in range(length)]
+    random_4 = [random.uniform(0, 25000000) for _ in range(length)]
+    return [get_fft_real(random_1), classifier(np.fft.fft(random_1), 10000, 200),
+            get_fft_real(random_2), classifier(np.fft.fft(random_2), 10000, 200),
+            get_fft_real(random_3), classifier(np.fft.fft(random_3), 10000, 200),
+            get_fft_real(random_4), classifier(np.fft.fft(random_4), 10000, 200)]
+
+def random_8_decimal (): # BIT_WIDTH = 32, DECIMAL_PT = 8
+    np.random.seed(42)
+    length = 16
+    random_1 = [random.uniform(0, 250000) for _ in range(length)]
+    random_2 = [random.uniform(0, 250000) for _ in range(length)]
+    random_3 = [random.uniform(0, 250000) for _ in range(length)]
+    random_4 = [random.uniform(0, 250000) for _ in range(length)]
+    return [get_fft_real(random_1), classifier(np.fft.fft(random_1), 10000, 200),
+            get_fft_real(random_2), classifier(np.fft.fft(random_2), 10000, 200),
+            get_fft_real(random_3), classifier(np.fft.fft(random_3), 10000, 200),
+            get_fft_real(random_4), classifier(np.fft.fft(random_4), 10000, 200)]
+
+def random_16_decimal (): # BIT_WIDTH = 32, DECIMAL_PT = 16
+    np.random.seed(42)
+    length = 16
+    random_1 = [random.uniform(0, 2500) for _ in range(length)]
+    random_2 = [random.uniform(0, 2500) for _ in range(length)]
+    random_3 = [random.uniform(0, 2500) for _ in range(length)]
+    random_4 = [random.uniform(0, 2500) for _ in range(length)]
+    return [get_fft_real(random_1), classifier(np.fft.fft(random_1), 10000, 200),
+            get_fft_real(random_2), classifier(np.fft.fft(random_2), 10000, 200),
+            get_fft_real(random_3), classifier(np.fft.fft(random_3), 10000, 200),
+            get_fft_real(random_4), classifier(np.fft.fft(random_4), 10000, 200)]
+
+def sine_wave_below_mag():
+    cutoff_mag = 20
+    sample_freq = 44000
+    frequencies = [100, 500, 1000, 5000]
+    sine_wave = [generate_sine_wave_below_mag(f, sample_freq, cutoff_mag) for f in frequencies]
+    return [get_fft_real(sine_wave[0]), classifier(np.fft.fft(sine_wave[0]), 10000, 200),
+            get_fft_real(sine_wave[1]), classifier(np.fft.fft(sine_wave[1]), 10000, 200),
+            get_fft_real(sine_wave[2]), classifier(np.fft.fft(sine_wave[2]), 10000, 200),
+            get_fft_real(sine_wave[3]), classifier(np.fft.fft(sine_wave[3]), 10000, 200)]
+
+# def random_sines():
+#     np.random.seed(42)
+#     length = 16
+#     sample_freq = 44000
+#     t = np.arange(0, length * (1/sample_freq), (1/sample_freq))
+#     rand_frequencies = [np.random.uniform(0, 20000) for _ in range(8)]
+#     rand_amplitudes = [np.random.uniform(0, 2000) for _ in range(8)]
+#     sine_wave = [a*np.sin(2 * np.pi * f * t) for a in rand_amplitudes for f in rand_frequencies]
+#     returning = []
+#     for i in range(64):
+#         returning.append(get_fft_real(sine_wave[i]))
+#         returning.append(classifier(np.fft.fft(sine_wave[i]), 10000, 200))
+#     return returning
 
 
 test_case_table = mk_test_case_table(
@@ -165,12 +249,15 @@ test_case_table = mk_test_case_table(
         ["below_freq_mag",         below_freq_mag,       4,        4,         32,       16,        16,       540672000,   655360,     44000,         False],
         ["above_freq_mag",         above_freq_mag,       4,        4,         32,       16,        16,       655360000,   1310720,    44000,         False],
         ["above_freq_below_mag",   above_freq_below_mag, 4,        4,         32,       16,        16,       655360000,   1310720,    44000,         False],
-        ["one_true",               one_true,             4,        4,         32,       16,        16,       655360000,   2621440,    44000,         False],
-        #["b_freq_l_amp_sin_1",   b_freq_l_amp_sin_1, 4,        4,         32,       16,        16,       32768000,   32768,     44000,         False],
-        #["b_freq_l_amp_sin_2",   b_freq_l_amp_sin_2, 4,        4,         32,       16,        16,       32768000,   32768,     44000,         False],
-        # ["sine_wave",   sine_wave, 4,        4,         32,       16,        16,       32768000,   32768,     44000,         False],
-        # ["false_test",   false_test, 4,        4,         32,       16,        16,       65536000,   1310720,   50000,         False],
-        # ["true_test",    true_test,                  4,        4,         32,       10,        16,       65536000,   1310720,   96000,        False],
+        ["random_4_decimal",       random_4_decimal,     4,        4,         32,       4 ,        16,       160000   ,   320    ,    44000,         False],
+        ["random_8_decimal",       random_8_decimal,     4,        4,         32,       8 ,        16,       2560000  ,   5120   ,    44000,         False],
+        ["random_16_decimal",      random_16_decimal,    4,        4,         32,       16,        16,       655360000,   1310720,    44000,         False],
+        ["sine_wave_below_mag",    sine_wave_below_mag,  4,        4,         32,       16,        16,       655360000,   1310720,    44000,         False],
+        ["input_delay_small",      above_freq_mag,       1,        4,         32,       16,        16,       655360000,   1310720,    44000,         False],
+        ["input_delay_big",        above_freq_mag,       8,        4,         32,       16,        16,       655360000,   1310720,    44000,         False],
+        ["output_delay_small",     above_freq_mag,       4,        1,         32,       16,        16,       655360000,   1310720,    44000,         False],
+        ["output_delay_small",     above_freq_mag,       4,        8,         32,       16,        16,       655360000,   1310720,    44000,         False],
+        #["random_sines",           random_sines,         4,        4,         32,       16,        16,       655360000,   1310720,    44000,         False],
     ]
 )
 
@@ -188,9 +275,10 @@ def test(test_params, cmdline_opts):
     # inputs = [fixed_bits(x) for sample in inputs for x in sample]
     # outputs = [x for x in outputs]
     msgs = test_params.msgs()
-    print(msgs)
     msgs = [make_arr_fixed(test_params.BIT_WIDTH, test_params.DECIMAL_PT, x) if i%2 != 0 else x for i, x in enumerate(msgs, start=1)]
     msgs = [mk_packed(test_params.BIT_WIDTH)(*x) if i%2 != 0 else x for i, x in enumerate(msgs, start=1)]
+
+    print(msgs[1::2])
 
     th.set_param(
         "top.src.construct",
