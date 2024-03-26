3caa2c2883cf9abb0726a1a90d26e6a73cfb4ad9
fl327 <fl327@cornell.edu>
2024-03-26 17:17:40 -0400

    butterfly tests

---
+++ src/fixed_point/combinational/butterfly.py
@@ -0,0 +1,35 @@
+from pymtl3 import *
+from pymtl3.stdlib.stream.ifcs import RecvIfcRTL, SendIfcRTL
+from pymtl3.passes.backends.verilog import *
+from os import path
+from tools.utils import mk_list_bitstruct
+from src.fixed_point.utils import mk_butterfly_input, mk_butterfly_output
+
+
+class Butterfly(VerilogPlaceholder, Component):
+    # Constructor
+
+    def construct(s, n, d, b):
+        # Interface
+        s.ar = [InPort(n) for _ in range(b)]
+        s.ac = [InPort(n) for _ in range(b)]
+        s.br = [InPort(n) for _ in range(b)]
+        s.bc = [InPort(n) for _ in range(b)]
+        s.wr = [InPort(n) for _ in range(b)]
+        s.wc = [InPort(n) for _ in range(b)]
+
+        s.cr = [OutPort(n) for _ in range(b)]
+        s.cc = [OutPort(n) for _ in range(b)]
+        s.dr = [OutPort(n) for _ in range(b)]
+        s.dc = [OutPort(n) for _ in range(b)]
+
+        # Source file path
+        # The ../ is necessary here because pytest is run from the build directory
+        s.set_metadata(
+            VerilogPlaceholderPass.src_file,
+            path.join(path.dirname(__file__), "butterfly.v"),
+        )
+
+        # Name of the top level module to be imported
+        s.set_metadata(VerilogPlaceholderPass.top_module, "Butterfly")
+
