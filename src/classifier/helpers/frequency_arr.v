b1ffde7ea3c3cde627807192e622667b8ae882fa
tic3 <tic3@cornell.edu>
2024-04-03 15:02:26 -0400

    Modified freq array gen to only output positive freqs

--- src/classifier/helpers/frequency_arr.v
+++ src/classifier/helpers/frequency_arr.v
@@ -14,16 +14,12 @@ module frequency_arr #(
   output logic [BIT_WIDTH - 1:0] frequency_out[N_SAMPLES - 1:0]
 );
 
-  localparam int SCALING_FACTOR = 1 << DECIMAL_PT;
   localparam int FREQUENCY_STEP = SAMPLING_FREQUENCY / N_SAMPLES;
 
   genvar i;
   generate
     for (i = 0; i < N_SAMPLES; i++) begin : gen_freq
-      // Calculating index for frequency bin, considering positive and negative frequencies.
-      localparam int index = (i < N_SAMPLES / 2) ? i : i - N_SAMPLES;
-      // Assigning pre-calculated frequency values to the output array.
-      assign frequency_out[i] = (index * FREQUENCY_STEP) << DECIMAL_PT;
+      assign frequency_out[i] = (i * FREQUENCY_STEP) << DECIMAL_PT;
     end
   endgenerate
 
