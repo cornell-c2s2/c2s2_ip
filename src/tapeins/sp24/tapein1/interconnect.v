26d37af20c42019623e52c23a834b2f266d6e605
vml37 <vml37@cornell.edu>
2024-04-09 18:04:52 -0400

    added router

--- src/tapeins/sp24/tapein1/interconnect.v
+++ src/tapeins/sp24/tapein1/interconnect.v
@@ -1,4 +1,5 @@
 `include "spi/minion.v"
+`include "arbiter_router/router.v"
 
 module tapeins_sp24_tapein1_Interconnect (
   input  logic clk,
@@ -8,6 +9,13 @@ module tapeins_sp24_tapein1_Interconnect (
   output logic miso,
   input  logic sclk
 );
+  logic [BIT_WIDTH - 1:0] recv_msg;
+  logic                   recv_rdy;
+  logic                   recv_val;
+  logic [BIT_WIDTH - 1:0] send_msg;
+  logic                   send_rdy;
+  logic                   send_val;
+
   spi_Minion #(
     .nbits(20)
   ) minion (
@@ -16,6 +24,30 @@ module tapeins_sp24_tapein1_Interconnect (
     .cs(cs),
     .mosi(mosi),
     .miso(miso),
-    .sclk(sclk)
+    .sclk(sclk),
+    .recv_msg(recv_msg),
+    .recv_rdy(recv_rdy),
+    .recv_val(recv_val),
+    .send_msg(send_msg),
+    .send_rdy(send_rdy),
+    .send_val(send_val),
+    .minion_parity(minion_parity),
+    .adapter_parity(adapter_parity)
+
   );
+
+  arbiter_router_Router #(
+    .nbits(20),
+    .noutputs(2)
+  ) router (
+    .clk(clk),
+    .reset(reset),
+    .istream_val(),
+    .istream_msg(),
+    .istream_rdy(),
+    .ostream_val(),
+    .ostream_msg(),
+    .ostream_rdy()
+  );
+
 endmodule
