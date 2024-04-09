fcf3f5da8455196bf3a6a0e95455fc892c672b41
el595 <el595@cornell.edu>
2024-04-09 17:35:57 -0400

    hook up part of minion

--- src/tapeins/sp24/tapein1/interconnect.v
+++ src/tapeins/sp24/tapein1/interconnect.v
@@ -6,20 +6,16 @@ module tapeins_sp24_tapein1_Interconnect (
   input  logic cs,
   input  logic mosi,
   output logic miso,
-  input  logic sclk,
+  input  logic sclk
 );
-  spi_Minion #(.nbits(20)) minion (
-    
-  input  logic             clk,
-  input  logic             cs,
-  output logic             miso,
-  input  logic             mosi,
-  input  logic             reset,
-  input  logic             sclk,
-  output logic             pull_en,
-  input  logic [nbits-1:0] pull_msg,
-  output logic             push_en,
-  output logic [nbits-1:0] push_msg,
-  output logic             parity
+  spi_Minion #(
+    .nbits(20)
+  ) minion (
+    .clk(clk),
+    .reset(reset),
+    .cs(cs),
+    .mosi(mosi),
+    .miso(miso),
+    .sclk(sclk)
   );
 endmodule
