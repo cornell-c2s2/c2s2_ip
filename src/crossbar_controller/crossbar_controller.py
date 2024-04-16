84247bcbfb168946fe87d32e6e2200bbb4f76037
d-gav <deg273@cornell.edu>
2024-04-16 18:19:28 -0400

    Fixed python wrapper.

--- src/crossbar_controller/crossbar_controller.py
+++ src/crossbar_controller/crossbar_controller.py
@@ -1,5 +1,5 @@
 from pymtl3 import *
-from pymtl3.stdlib.stream.ifcs import RecvIfcRTL, SendIfcRTL
+from pymtl3.stdlib import stream
 from pymtl3.passes.backends.verilog import *
 from os import path
 from tools.utils import mk_list_bitstruct
@@ -8,6 +8,11 @@ from tools.utils import mk_list_bitstruct
 class CrossbarController(VerilogPlaceholder, Component):
     # Constructor
     def construct(s):
+        s.indata = InPort(6)
+        s.w = InPort(1)
+        s.s = InPort(1)
+        s.defaultout = OutPort(6)
+
         s.set_metadata(
             VerilogTranslationPass.explicit_module_name, "crossbar_controller"
         )
