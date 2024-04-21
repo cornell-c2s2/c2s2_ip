51c2e131736f4c16deec6573e928cdd730119a69
d-gav <deg273@cornell.edu>
2024-04-21 14:40:30 -0400

    Added .sby and new controller implementation.

--- src/crossbar_controller/crossbar_controller.v
+++ src/crossbar_controller/crossbar_controller.v
@@ -1,28 +1,34 @@
-module crossbar_controller (
-  indata,
-  w,
-  s,
-  defaultout
-);
-  input [5:0] in;
-  input w;
-  input s;
+module crossbar_controller(in, w, s, defaultout);
+    input[5:0] in;
+    input w;
+    input s;
 
-  output [5:0] defaultout;
-  reg defaultout;
+    output[5:0] defaultout;
+    reg defaultout;
 
-  /*
+    /*
     Outputs 6b control bus of 2 1-hot 3b strings, 1 for input and 1 for output.
     For each 3b wire:
     default, both pins low --> 100
     SPI pin high --> 010
     GDS pin high --> 001
      */
+    /*
+    wire [2:0] tempin;
+    wire [2:0] tempout;
 
-  always @(indata, w, s) begin
-    if (w == 1'b1) defaultout <= 6'b001001;
-    else if (s == 1'b1) defaultout <= 6'b010010;
-    else defaultout <= indata;
-  end
+    assign tempin = {~(W[1] | S[1]), S[1], W[1]};
+    assign tempout = {~(W[0] | S[0]), S[0], W[0]};
+    assign defaultout = {tempin, tempout};
+    */
 
-endmodule
+    always_comb @(*) begin
+        if(w==1'b1)
+            defaultout = 6'b001001;
+        else if(s==1'b1)
+            defaultout = 6'b010010;
+        else
+            defaultout = in;
+    end
+
+  endmodule
