a041d2c99c667a7557778b5ff2ac379014f1d73c
fl327 <fl327@cornell.edu>
2024-05-07 16:47:59 -0400

    spi tests loopback

--- src/tapeins/sp24/tapein1/tests/test_interconnect.py
+++ src/tapeins/sp24/tapein1/tests/test_interconnect.py
@@ -13,11 +13,11 @@ from pymtl3.stdlib.test_utils import (
 max_trsns = 100
 
 
-@pytest.mark.parametrize(
-    "in_msgs, out_msgs",
-    [([0x15555, 0x2AAAA], [0x15555, 0x2AAAA])],
-)
-def test_interconnect(in_msgs, out_msgs, cmdline_opts):
+# @pytest.mark.parametrize(
+#     "in_msgs, out_msgs",
+#     [([0x15555, 0x2AAAA], [0x2CAFE, 0x2CAFE])],
+# )
+def run_interconnect(in_msgs, out_msgs, cmdline_opts):
     dut = Interconnect()
     dut = config_model_with_cmdline_opts(dut, cmdline_opts, duts=[])
     dut.apply(DefaultPassGroup(linetrace=False))
@@ -32,17 +32,21 @@ def test_interconnect(in_msgs, out_msgs, cmdline_opts):
 
     spc = 0
 
+    print("\nBEGIN TEST\n=============================")
+
     while out_idx < len(out_msgs):
         if trsns > max_trsns:
             assert False, "Exceeded max transactions"
 
         if in_idx < len(in_msgs) and spc == 1:
             retmsg = spi_write(dut, write_read_msg(in_msgs[in_idx]))
+            print("Trsn ", trsns, " SENT: ", in_msgs[in_idx])
             in_idx += 1
             spc = retmsg[18]
             if retmsg[19] == 1:
                 assert retmsg[0:18] == out_msgs[out_idx]
                 out_idx += 1
+                print("Trsn ", trsns, " RECV: ", retmsg[0:18])
 
         else:
             retmsg = spi_write(dut, nocommand_read_msg())
@@ -50,5 +54,16 @@ def test_interconnect(in_msgs, out_msgs, cmdline_opts):
             if retmsg[19] == 1:
                 assert retmsg[0:18] == out_msgs[out_idx]
                 out_idx += 1
+                print("Trsn ", trsns, " RECV: ", retmsg[0:18])
 
         trsns += 1
+
+
+# @pytest.mark.parametrize(
+#     "msgs",
+#     [[0x15555, 0x2AAAA], [0x2AAAA, 0x15555]],
+# )
+def test_loopback(cmdline_opts):
+    in_msgs = [0x20000, 0x15555, 0x2AAAA]
+    out_msgs = [0x15555, 0x2AAAA]
+    run_interconnect(in_msgs, out_msgs, cmdline_opts)
