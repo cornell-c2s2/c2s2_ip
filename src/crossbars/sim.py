215437b7718336a5e79b0f0b4785cc2b70a427fb
d-gav <deg273@cornell.edu>
2024-05-09 14:41:53 -0400

    Wrote tests including new spi config wires

--- src/crossbars/sim.py
+++ src/crossbars/sim.py
@@ -11,9 +11,12 @@ def create_crossbar(nbits: int, nin: int, nout: int) -> tuple[callable, callable
         # All outputs are zero except for the selected output
         return outputs
 
-    def configure(_in_sel: int, _out_sel: int):
+    def configure(_in_sel: int, _out_sel: int, input_spi: int, output_spi: int):
         nonlocal in_sel, out_sel
+        if (input_spi == 1): in_sel = 0
+        if (output_spi == 1): out_sel = 0
         in_sel = _in_sel
         out_sel = _out_sel
+
 
     return crossbar, configure
