2ebfdfa7b7472fc45ba19a2c6a4786d55be301dc
fl327 <fl327@cornell.edu>
2024-05-10 22:48:02 -0400

    fft tests

--- src/tapeins/sp24/tapein1/tests/test_interconnect.py
+++ src/tapeins/sp24/tapein1/tests/test_interconnect.py
@@ -2,6 +2,10 @@ import pytest
 from src.tapeins.sp24.tapein1.tests.spi_driver_sim import spi_write
 from src.tapeins.sp24.tapein1.interconnect import Interconnect
 from src.tapeins.sp24.tapein1.tests.spi_stream_protocol import *
+from fixedpt import Fixed, CFixed
+from tools.utils import fixed_bits
+
+# from src.fft.tests.fft import *
 
 from pymtl3 import *
 from pymtl3.stdlib.test_utils import (
@@ -13,10 +17,6 @@ from pymtl3.stdlib.test_utils import (
 max_trsns = 100
 
 
-# @pytest.mark.parametrize(
-#     "in_msgs, out_msgs",
-#     [([0x15555, 0x2AAAA], [0x2CAFE, 0x2CAFE])],
-# )
 def run_interconnect(in_msgs, out_msgs, cmdline_opts):
     dut = Interconnect()
     dut = config_model_with_cmdline_opts(dut, cmdline_opts, duts=[])
@@ -49,7 +49,7 @@ def run_interconnect(in_msgs, out_msgs, cmdline_opts):
                 print("Trsn ", trsns, " RECV: ", retmsg[0:18])
 
         else:
-            retmsg = spi_write(dut, nocommand_read_msg())
+            retmsg = spi_write(dut, read_msg())
             spc = retmsg[18]
             if retmsg[19] == 1:
                 assert retmsg[0:18] == out_msgs[out_idx]
@@ -59,11 +59,92 @@ def run_interconnect(in_msgs, out_msgs, cmdline_opts):
         trsns += 1
 
 
-# @pytest.mark.parametrize(
-#     "msgs",
-#     [[0x15555, 0x2AAAA], [0x2AAAA, 0x15555]],
-# )
-def test_loopback(cmdline_opts):
-    in_msgs = [0x20000, 0x15555, 0x2AAAA]
-    out_msgs = [0x15555, 0x2AAAA]
+class InXbarCfg(int):
+    SPI_SPI = 0b00  # Loopback
+    SPI_FFT = 0b01  # FFT
+
+
+class OutXbarCfg(int):
+    SPI_SPI = 0b00  # Loopback
+    FFT_SPI = 0b10  # FFT
+
+
+def input_xbar_config_msg(input: InXbarCfg):
+    assert input < 4 and input >= 0
+    return 0x20000 | input
+
+
+def output_xbar_config_msg(output: OutXbarCfg):
+    assert output < 4 and output >= 0
+    return 0x30000 | output
+
+
+@pytest.mark.parametrize(
+    "msgs",
+    [
+        [0xFFFF],
+        [0x0000],
+        [0x5555, 0xAAAA],
+        [0xAAAA, 0x5555],
+        [0xDEAD, 0xBEEF, 0xCAFE, 0xBABE],
+        [0xABCD, 0x1234, 0x5678, 0x9ABC, 0xDEF0],
+    ],
+)
+def test_loopback_inXbar(msgs, cmdline_opts):
+    # make sure all messages are 16 bits
+    for msg in msgs:
+        assert msg < 0x10000 and msg >= 0
+    in_msgs = [input_xbar_config_msg(InXbarCfg.SPI_SPI)] + msgs
+    out_msgs = msgs
     run_interconnect(in_msgs, out_msgs, cmdline_opts)
+
+
+@pytest.mark.parametrize(
+    "msgs",
+    [
+        [0xFFFF],
+        [0x0000],
+        [0x5555, 0xAAAA],
+        [0xAAAA, 0x5555],
+        [0xDEAD, 0xBEEF, 0xCAFE, 0xBABE],
+        [0xABCD, 0x1234, 0x5678, 0x9ABC, 0xDEF0],
+    ],
+)
+def test_loopback_outXbar(msgs, cmdline_opts):
+    for msg in msgs:
+        assert msg < 0x10000 and msg >= 0
+    in_msgs = [
+        output_xbar_config_msg(OutXbarCfg.SPI_SPI),
+    ] + msgs
+    out_msgs = msgs
+    run_interconnect(in_msgs, out_msgs, cmdline_opts)
+
+
+def run_fft(inputs: list[Fixed], outputs: list[Fixed], cmdline_opts):
+
+    inputs = [fixed_bits(x) for sample in inputs for x in sample]
+    outputs = [fixed_bits(x) for sample in outputs for x in sample]
+
+    in_msgs = [
+        input_xbar_config_msg(InXbarCfg.SPI_FFT),
+        output_xbar_config_msg(OutXbarCfg.FFT_SPI),
+    ] + [int(x) for x in inputs]
+
+    out_msgs = [(0x10000 | int(x)) for x in outputs]
+
+    run_interconnect(in_msgs, out_msgs, cmdline_opts)
+
+
+def fixN(n):
+    return Fixed(n, True, 16, 8)
+
+
+@pytest.mark.parametrize(
+    "input, output",
+    [
+        ([[fixN(1) for _ in range(32)]], [[fixN(32)] + [fixN(0) for _ in range(31)]]),
+    ],
+)
+def test_fft_manual(input, output, cmdline_opts):
+    print(fixN(1))
+    run_fft(input, output, cmdline_opts)
