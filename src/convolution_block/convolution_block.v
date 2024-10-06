01a1ccd26bbe2fb55b3a82017e4286967ea34788
Arjun Saini <69866165+Arjun-Saini@users.noreply.github.com>
2024-10-06 13:23:59 -0400

    Write initial convolution block implementation

--- src/convolution_block/convolution_block.v
+++ src/convolution_block/convolution_block.v
@@ -6,22 +6,41 @@
 `define CONVOLUTION_BLOCK_V
 
 module convolution_block_ConvolutionBlock #(
-  parameter int Width = 32
+  parameter int BIT_WIDTH = 32,
+  parameter int ARRAY_LENGTH = 8
 ) (
   input logic clk,
   input logic reset,
 
-  output logic recv_rdy,
-  input logic recv_val,
-  input logic [Width - 1:0] recv_msg,
+  // input array
+  output logic input_arr_rdy,
+  input logic input_arr_val,
+  input logic [BIT_WIDTH - 1:0] input_arr_msg[ARRAY_LENGTH - 1:0],
 
-  input logic send_rdy,
-  output logic send_val,
-  output logic [Width - 1:0] send_msg
+  // input filter
+  output logic filter_rdy,
+  input logic filter_val,
+  input logic [BIT_WIDTH - 1:0] filter_msg[ARRAY_LENGTH - 1:0],
+
+  // output array
+  input logic output_arr_rdy,
+  output logic output_arr_val,
+  output logic [BIT_WIDTH - 1:0] output_arr_msg[ARRAY_LENGTH - 1:0]
 );
+
+  // assign val and rdy bits
   always_comb begin
-    send_msg = recv_msg;
+    input_arr_rdy = output_arr_rdy;
+    filter_rdy = output_arr_rdy;
+    output_arr_val = input_arr_val & filter_val;
   end
+
+  // perform array convolution: output[i] = input[i] * filter[n - i]
+  generate
+    for (genvar i = 0; i < ARRAY_LENGTH; i++) begin
+      assign output_arr_msg[i] = input_arr_msg[i] * filter_msg[ARRAY_LENGTH-i];
+    end
+  endgenerate
 endmodule
 
 `endif
