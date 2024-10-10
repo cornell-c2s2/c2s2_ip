90ebd89511cca76c3fd686684cfd076be3e88659
mmcgruder213 <michaeljr.unclesam@gmail.com>
2024-10-09 22:04:58 -0400

    Fixed directed test to represent fixed point numbers more seemlessly

--- src/fixed_point/iterative/test_cocotb/test_FXPIterMultDatapath.py
+++ src/fixed_point/iterative/test_cocotb/test_FXPIterMultDatapath.py
@@ -5,6 +5,7 @@ import subprocess
 from pathlib import Path
 
 import cocotb
+from fxpmath.objects import Fxp
 from cocotb.triggers import Timer, RisingEdge, FallingEdge
 from cocotb.runner import get_runner
 
@@ -20,35 +21,38 @@ def update_control(dut, counter):
     if (counter == 31):
         dut.do_carry.value = 1
     else:
-        dut.do_carry.value = 0   
+        dut.do_carry.value = dut.do_carry.value   
 
 #Single directed test
 @cocotb.test()
 async def datapath_basic_test(dut):
     #Set initial values of signals
-    dut.a.value = 15
-    dut.b.value = 13
+    A = Fxp(-5.25, signed=True, n_word=48, n_frac=16)
+    B = Fxp(3.75, signed=True, n_word=32, n_frac=16)
+    dut.a.value = int(A.bin(), 2)
+    dut.b.value = int(B.bin(), 2)
     dut.in_wait.value = 1
     dut.reset.value = 1
     dut.do_add.value = 0
     dut.do_carry.value = 0
-    
+
     #Start clock
     await cocotb.start(generate_clock(dut))
     await RisingEdge(dut.clk)
-    #Start the operation of the control path model
+    #End reset
     dut.reset.value = 0
-    dut.do_add.value = 1
     await RisingEdge(dut.clk)
+    #Start 
+    dut.do_add.value = 1
     dut.in_wait.value = 0
     await FallingEdge(dut.clk)
     for x in range(32):
-        await RisingEdge(dut.clk)
         update_control(dut, x)
+        await RisingEdge(dut.clk)
         await FallingEdge(dut.clk)
-
     #Check the value returned by the datapath
-    assert int(dut.c.value) == 195, "C not equal to 195"
+    C = Fxp(-5.25*3.75, signed=True, n_word=32, n_frac=16)
+    assert dut.c.value == int(C.bin(),2), "C not equal to 195"
 
 @cocotb.test()
 async def datapath_reset_test(dut):
