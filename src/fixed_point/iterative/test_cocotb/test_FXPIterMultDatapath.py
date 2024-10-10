63bac38bb45083260ec59691492f1a896f5681cc
mmcgruder213 <michaeljr.unclesam@gmail.com>
2024-10-10 11:11:50 -0400

    Made small corrections to control path model and added randomized tests

--- src/fixed_point/iterative/test_cocotb/test_FXPIterMultDatapath.py
+++ src/fixed_point/iterative/test_cocotb/test_FXPIterMultDatapath.py
@@ -1,3 +1,5 @@
+import pytest
+
 import os
 import random
 import sys
@@ -6,7 +8,7 @@ from pathlib import Path
 
 import cocotb
 from fxpmath.objects import Fxp
-from cocotb.triggers import Timer, RisingEdge, FallingEdge
+from cocotb.triggers import Timer, RisingEdge
 from cocotb.runner import get_runner
 
 #Generate clock pulses (50 cycles)
@@ -17,18 +19,24 @@ async def generate_clock(dut):
         dut.clk.value = 1
         await Timer(1, units="ns")
 
+#Check if expected value of c is the same as the actual value
+#TODO:Account for overflow
+def equal(actual, expected):
+    return actual == expected or actual == expected-1
+#Updating the state of the multiplier
 def update_control(dut, counter):
     if (counter == 31):
         dut.do_carry.value = 1
-    else:
-        dut.do_carry.value = dut.do_carry.value   
+    elif (counter == 32): #Transition from CALC to DONE
+        dut.do_add.value = 0
+        dut.do_carry.value = 0 
 
-#Single directed test
-@cocotb.test()
-async def datapath_basic_test(dut):
+#Reset then multiply coroutine (model for control path)
+async def reset_then_mult(dut, a, b): 
     #Set initial values of signals
-    A = Fxp(-5.25, signed=True, n_word=48, n_frac=16)
-    B = Fxp(3.75, signed=True, n_word=32, n_frac=16)
+    A = Fxp(a, signed=True, n_word=32, n_frac=16, rounding='around')
+    A.resize(n_word=48, n_frac=16)
+    B = Fxp(b, signed=True, n_word=32, n_frac=16, rounding='around')
     dut.a.value = int(A.bin(), 2)
     dut.b.value = int(B.bin(), 2)
     dut.in_wait.value = 1
@@ -39,28 +47,54 @@ async def datapath_basic_test(dut):
     #Start clock
     await cocotb.start(generate_clock(dut))
     await RisingEdge(dut.clk)
-    #End reset
+
+    #End reset and spend whole cycle in IDLE
     dut.reset.value = 0
     await RisingEdge(dut.clk)
-    #Start 
+    await RisingEdge(dut.clk)
+    #Transition from IDLE to CALC
     dut.do_add.value = 1
     dut.in_wait.value = 0
-    await FallingEdge(dut.clk)
-    for x in range(32):
+    update_control(dut, 0)
+    await RisingEdge(dut.clk)
+    #Remain in CALC for 32 cycles
+    for x in range(1,33):
         update_control(dut, x)
         await RisingEdge(dut.clk)
-        await FallingEdge(dut.clk)
+
+#Single directed test
+@cocotb.test()
+async def datapath_basic_test(dut):
+    #Reset multiplier then multiply inputs
+    A = 5.37232
+    B = 4.7883
+    await reset_then_mult(dut, A, B)
+
     #Check the value returned by the datapath
-    C = Fxp(-5.25*3.75, signed=True, n_word=32, n_frac=16)
-    assert dut.c.value == int(C.bin(),2), "C not equal to 195"
+    C = Fxp(A, signed=True, n_word=48, n_frac=16, rounding='around')*Fxp(B, signed=True, n_word=32, n_frac=16, rounding='around')
+    C.resize(n_word=32, n_frac=16)
+    assert equal(int(dut.c.value), int(C.bin(), 2)), "C not correct" 
 
 @cocotb.test()
 async def datapath_reset_test(dut):
     pass
 
 @cocotb.test()
-async def datapath_randomised_test(dut):
-    pass
+async def datapath_randomized_test(dut):
+    for i in range(1000):
+        #Reset multiplier then multiply inputs
+        A = random.uniform(-150, 150)
+        B = random.uniform(-150, 150)
+        await reset_then_mult(dut, A, B)
+
+        #Check the value returned by the datapath
+        C = Fxp(A, signed=True, n_word=48, n_frac=16, rounding='around')*Fxp(B, signed=True, n_word=32, n_frac=16, rounding='around')
+        C.resize(n_word=32, n_frac=16)
+
+        if not equal(int(dut.c.value), int(C.bin(), 2)):
+            print("A = %f and B = %f" % (A, B))
+
+        assert equal(int(dut.c.value), int(C.bin(), 2)), "C not correct" 
 
 def test_datapath_runner():
     """Simulate the multiplier datapath using the Python runner.
