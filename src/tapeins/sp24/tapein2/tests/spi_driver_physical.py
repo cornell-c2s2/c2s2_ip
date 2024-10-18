70352066e572c9e3dc114c5afcb44e2df94db878
fl327 <fl327@cornell.edu>
2024-10-18 16:21:52 -0400

    format

--- src/tapeins/sp24/tapein2/tests/spi_driver_physical.py
+++ src/tapeins/sp24/tapein2/tests/spi_driver_physical.py
@@ -6,7 +6,6 @@ s = SPIDriver("/dev/ttyUSB0")
 def spi_write_physical(dut, src_msg):
     src_msg_bytes = []
 
-
     while src_msg.nbits > 8:
         src_msg_bytes.append(int(src_msg[src_msg.nbits - 8:src_msg.nbits]))
         src_msg = src_msg[:src_msg.nbits - 8]
