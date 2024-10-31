258add26698905345eec99aaefd6eb3e93016f7f
Arjun Saini <69866165+Arjun-Saini@users.noreply.github.com>
2024-10-31 14:00:04 -0400

    Randomized delay added to tests

--- src/convolution_block/combinational/convolution_block_combinational.v
+++ src/convolution_block/combinational/convolution_block_combinational.v
@@ -13,20 +13,20 @@ module convolution_block_combinational_ConvolutionBlock #(
   input logic reset,
 
   // input array
-  input  logic [BIT_WIDTH - 1:0] input_msg [ARRAY_LENGTH - 1:0],
+  input  logic [BIT_WIDTH - 1:0] req1_msg[ARRAY_LENGTH - 1:0],
 
-  // input filter
-  input  logic [BIT_WIDTH - 1:0] filter_msg[ARRAY_LENGTH - 1:0],
+  // input filter (array that gets reversed)
+  input  logic [BIT_WIDTH - 1:0] req2_msg[ARRAY_LENGTH - 1:0],
 
   // output array
-  output logic [BIT_WIDTH - 1:0] output_msg[ARRAY_LENGTH - 1:0]
+  output logic [BIT_WIDTH - 1:0] resp_msg[ARRAY_LENGTH - 1:0]
 );
 
   // perform array convolution: output[i] = input[i] * filter[n - i]
   generate
     for (genvar i = 0; i < ARRAY_LENGTH; i++) begin
       always @(posedge clk) begin
-        output_msg[i] <= reset ? 0 : input_msg[i] * filter_msg[ARRAY_LENGTH-i-1];
+        resp_msg[i] <= reset ? 0 : req1_msg[i] * req2_msg[ARRAY_LENGTH-i-1];
       end
     end
   endgenerate
