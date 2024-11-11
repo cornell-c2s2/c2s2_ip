fb3df114588aabba5e51bf3ab2a661022af55c92
jjm469 <jjm469@cornell.edu>
2024-11-11 17:51:50 -0500

    Initial IP: misr

---
+++ src/misr/cocotb_misr/misr.v
@@ -0,0 +1,27 @@
+//================================================
+// misr.v
+//================================================
+`default_nettype none
+`ifndef MISR_V
+`define MISR_V
+
+module misr_Misr #(
+  parameter int Width = 32
+) (
+  input logic clk,
+  input logic reset,
+
+  output logic recv_rdy,
+  input logic recv_val,
+  input logic [Width - 1:0] recv_msg,
+
+  input logic send_rdy,
+  output logic send_val,
+  output logic [Width - 1:0] send_msg
+);
+  always_comb begin
+    send_msg = recv_msg;
+  end
+endmodule
+
+`endif
