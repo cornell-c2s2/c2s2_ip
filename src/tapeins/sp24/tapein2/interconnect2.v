72c5bdea8cd3c936a6a534df0a79a23405483d7a
mmcgruder213 <michaeljr.unclesam@gmail.com>
2024-11-19 16:25:39 -0500

    Removed unused wires from interconnect2.v

--- src/tapeins/sp24/tapein2/interconnect2.v
+++ src/tapeins/sp24/tapein2/interconnect2.v
@@ -479,13 +479,8 @@ module tapeins_sp24_tapein2_Interconnect2 (
       wire unused_router_addr = &{1'b0, router_msg[i][DATA_BITS+ADDR_BITS-1:DATA_BITS], 1'b0};
     end
   endgenerate
-<<<<<<< HEAD
   //wire unused_router_val = &{1'b0, router_val[10:ROUTER_ARBITER_SIZE-1], 1'b0};
   //wire unused_router_msg = &{1'b0, router_msg[10:ROUTER_ARBITER_SIZE-1], 1'b0};
-=======
-  // wire unused_router_val = &{1'b0, router_val[10:ROUTER_ARBITER_SIZE-1], 1'b0};
-  // wire unused_router_msg = &{1'b0, router_msg[10:ROUTER_ARBITER_SIZE-1], 1'b0};
->>>>>>> refs/remotes/origin/cocotb-pathfinding
 
   // 5 outputs:
   // 0: input xbar output
@@ -526,7 +521,6 @@ module tapeins_sp24_tapein2_Interconnect2 (
     end
   endgenerate
 
-<<<<<<< HEAD
   //wire unused_arbiter_rdy = &{
   //  1'b0,
   //  arbiter_rdy[1],
@@ -534,16 +528,7 @@ module tapeins_sp24_tapein2_Interconnect2 (
   //  arbiter_rdy[5:8],
   //  arbiter_rdy[10:ROUTER_ARBITER_SIZE-1],
   //  1'b0
-=======
-  // wire unused_arbiter_rdy = &{
-  //   1'b0,
-  //   arbiter_rdy[1],
-  //   arbiter_rdy[3],
-  //   arbiter_rdy[5:8],
-  //   arbiter_rdy[10:ROUTER_ARBITER_SIZE-1],
-  //   1'b0
->>>>>>> refs/remotes/origin/cocotb-pathfinding
-  // };
+  //};
 
 endmodule
 
