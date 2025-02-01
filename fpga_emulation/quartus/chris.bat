ce355f9003174287b0bd93635f6f97439b30d8e4
Simeon Turner <simeon.turner34@gmail.com>
2025-02-01 13:08:49 -0500

    modified chris.bat

--- fpga_emulation/quartus/chris.bat
+++ fpga_emulation/quartus/chris.bat
@@ -51,9 +51,9 @@ IF %ARG_PASS%==0 (
 
 for /f "delims=" %%i in ('git rev-parse --show-toplevel') do set TOP_DIR=%%i
 set SRC_DIR=%TOP_DIR%\src
-set FPGA_DIR=%TOP_DIR%\fpga_emulation
-set BUILD_DIR=%FPGA_DIR%\build
-set SCRIPT_DIR=%FPGA_DIR%\scripts
+set QUARTUS_DIR=%TOP_DIR%\fpga_emulation\quartus
+set BUILD_DIR=%QUARTUS_DIR%\build
+set SCRIPT_DIR=%TOP_DIR%\fpga_emulation\scripts
 
 rem CONVERSION
 set src_emulation_dir=%SRC_DIR%\tapeins\sp24\fpga_emulation2
@@ -72,16 +72,16 @@ set CABLE_INDEX=1
 rem 1. Convert
 IF %CONVERT% == 1 (
     echo:
-    echo Converting tapein files.
+    echo Attempting tapein files conversion.
     echo:
 
     mkdir "%BUILD_DIR%"
     python %SCRIPT_DIR%\sine_wave_lookup_generator.py 16 8 32 %lookup_file%
-    python %SCRIPT_DIR%\pickel.py --src %SRC_DIR% --interconnect %interconnect_file% --out %pickled_file%
+    python %SCRIPT_DIR%\pickle.py --src %SRC_DIR% --interconnect %interconnect_file% --out %pickled_file%
     python %SCRIPT_DIR%\convert.py --vfile %pickled_file% --lookup %lookup_file% --out %FPGA_DIR%\interconnect_fpga.v
 
     echo:
-    echo File converted successfully.
+    echo File conversion attempt finished.
     echo:
 )
 
@@ -89,18 +89,13 @@ IF %CONVERT% == 1 (
 rem 2: Execute the TCL script through quartus_sh
 IF %COMPILE% == 1 (
     echo:
-    echo Executing TCL script: %TCL_SCRIPT%
+    echo Attempting TCL script execution: %TCL_SCRIPT%
     echo:
 
     C:\intelFPGA_lite\23.1std\quartus\bin64\quartus_sh -t %TCL_SCRIPT%
 
-    if %ERRORLEVEL% neq 0 (
-        echo Error running TCL script. Aborting.
-        exit /b 1
-    )
-
     echo:
-    echo TCL script executed successfully.
+    echo TCL script execution attempt finished.
     echo:
 )
 
@@ -108,31 +103,26 @@ IF %COMPILE% == 1 (
 rem 3: Program the FPGA with quartus_pgm
 IF %PROGRAM% == 1 (
     echo:
-    echo Programming FPGA with SOF file: %SOF_FILE%
+    echo Attempting FPGA programming with SOF file: %SOF_FILE%
     echo:
 
     C:\intelFPGA_lite\23.1std\quartus\bin64\quartus_pgm -m JTAG -o "p;%SOF_FILE%@%CABLE_INDEX%"
 
-    if %ERRORLEVEL% neq 0 (
-        echo Error programming FPGA. Aborting.
-        exit /b 1
-    )
-
     echo:
-    echo FPGA programming completed successfully.
+    echo FPGA programming attempt finished.
     echo:
 )
 
 rem 4: Completely clear out all generated files
 IF %CLEAN% == 1 (
-    del "%FPGA_DIR%\interconnect_fpga.v"
-    del "%FPGA_DIR%\*.qpf"
-    del "%FPGA_DIR%\*.qsf"
-    del "%FPGA_DIR%\*.qws"
-    del "%FPGA_DIR%\*.bak"
-    rmdir /s /q "%FPGA_DIR%\build"
-    rmdir /s /q "%FPGA_DIR%\db"
-    rmdir /s /q "%FPGA_DIR%\incremental_db"
-    rmdir /s /q "%FPGA_DIR%\log"
-    rmdir /s /q "%FPGA_DIR%\output_files"
+    del "%QUARUTS_DIR%\interconnect_fpga.v"
+    del "%QUARUTS_DIR%\*.qpf"
+    del "%QUARUTS_DIR%\*.qsf"
+    del "%QUARUTS_DIR%\*.qws"
+    del "%QUARUTS_DIR%\*.bak"
+    rmdir /s /q "%QUARTUS_DIR%\build"
+    rmdir /s /q "%QUARTUS_DIR%\db"
+    rmdir /s /q "%QUARTUS_DIR%\incremental_db"
+    rmdir /s /q "%QUARTUS_DIR%\log"
+    rmdir /s /q "%QUARTUS_DIR%\output_files"
 )
