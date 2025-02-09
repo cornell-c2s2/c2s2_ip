ea9c5bf0c4f7d7ad424e61825c4db5ab5cc831bc
deg273 <deg273@cornell.edu>
2025-02-09 12:56:09 -0500

    .tcl file for MISR formal verification

--- src/lbist/misr/tests/formal/misr_sva.sv
+++ src/lbist/misr/tests/formal/misr_sva.sv
@@ -1,4 +1,4 @@
-`include "src/lbist/misr/misr.v" 
+`include "lbist/misr/misr.v" 
 
 module misr_sva #(
   parameter int CUT_MSG_BITS = 32,
@@ -13,16 +13,16 @@ module misr_sva #(
   // CUT to MISR
   input  logic                      cut_req_val,
   input  logic [CUT_MSG_BITS-1:0]   cut_req_msg,
-  output logic                      cut_req_rdy,
+  input  logic                      cut_req_rdy,
 
   // LBIST CONTROLLER to MISR
-  input logic                       lbist_req_val,
-  input logic  [LBIST_MSG_BITS:0]   lbist_req_msg,
-  output logic                      lbist_req_rdy,
+  input  logic                       lbist_req_val,
+  input  logic  [LBIST_MSG_BITS:0]   lbist_req_msg,
+  input  logic                      lbist_req_rdy,
 
   // MISR to LBIST CONTROLLER
-  output logic                      lbist_resp_val,
-  output logic [SIGNATURE_BITS-1:0] lbist_resp_msg,
+  input  logic                      lbist_resp_val,
+  input  logic [SIGNATURE_BITS-1:0] lbist_resp_msg,
   input  logic                      lbist_resp_rdy
 );
 
@@ -30,19 +30,29 @@ module misr_sva #(
 logic [SIGNATURE_BITS-1:0] hashes[MAX_OUTPUTS_TO_HASH-1:0];
 always_ff @(posedge clk) begin
   if (reset) begin
-    hashes <= '0;
+    hashes <= '{default: '0};
   end else if (lbist_resp_val && lbist_resp_rdy) begin
       hashes[lbist_req_msg] <= lbist_resp_msg;
   end
 end
 
-// validate that all the hashes generated are unique
+/*// validate that all the hashes are the same after a reset
 property unique_hashes;
   @(posedge clk)
   disable iff (!reset)
   (lbist_resp_val && lbist_resp_rdy) |-> 
   (hashes[lbist_req_msg] != lbist_resp_msg);
 endproperty
+*/
+
+property p_idle_to_hash;
+  @(posedge clk) disable iff (reset)
+    ( (!cut_req_rdy && lbist_req_rdy && !lbist_resp_val && lbist_req_val) )
+      |-> ##1 ( (cut_req_rdy && !lbist_req_rdy && !lbist_resp_val) );
+endproperty
+
+assert property (p_idle_to_hash);
+
 
 // Val/Rdy Properties
   // CUT interface
@@ -85,7 +95,7 @@ endproperty
   endproperty
 
   // Assertions
-  unique_hashes_assert:         assert property(unique_hashes);
+ // unique_hashes_assert:         assert property(unique_hashes);
   cut_val_rdy_handshake:        assert property(cut_val_rdy_stable);
   cut_msg_stable_assert:        assert property(cut_msg_stable);
   lbist_req_val_rdy_handshake:  assert property(lbist_req_val_rdy_stable);
@@ -96,9 +106,16 @@ endproperty
 
 endmodule
 
-bind misr_sva misr misr_inst (
-    .clk(clk),
-    .reset(reset),
-    .data_in(data_in),
-    .data_out(data_out)
+bind misr misr_sva u_misr_sva (
+  .clk(clk),
+  .reset(reset),
+  .cut_req_val(cut_req_val),
+  .cut_req_msg(cut_req_msg),
+  .cut_req_rdy(cut_req_rdy),
+  .lbist_req_val(lbist_req_val),
+  .lbist_req_msg(lbist_req_msg),
+  .lbist_req_rdy(lbist_req_rdy),
+  .lbist_resp_val(lbist_resp_val),
+  .lbist_resp_msg(lbist_resp_msg),
+  .lbist_resp_rdy(lbist_resp_rdy)
 );
---
+++ src/lbist/misr/tests/formal/run.tcl
@@ -0,0 +1,17 @@
+
+# this clears previous blocks and proofs
+clear -all
+ 
+# this says to initiate coverage checking, and allows you to list the modes (can also be done in gui)
+check_cov -init -type all -model {branch toggle statement} -toggle_ports_only
+ 
+# you must analyze all the files in your design (source and testbench) before doing anything in jasper
+analyze -sv lbist/misr/misr.v lbist/misr/tests/formal/misr_sva.sv
+ 
+# specify the top level module, in this case the testbench
+elaborate -top misr_sva -create_related_covers {precondition witness}
+ 
+# specify clock and reset signals
+clock clk
+# reset reset
+prove -bg -task {<embedded>}
