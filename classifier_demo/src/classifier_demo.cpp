a6a17956d272d93b6038d1c33eaaf69ab9987bc7
Nathan Rakhlin <nrakhlin@dhcp-vl2042-3373.redrover.cornell.edu>
2025-02-13 18:29:47 -0500

    outline of classifier_demo started

--- classifier_demo/src/classifier_demo.cpp
+++ classifier_demo/src/classifier_demo.cpp
@@ -1,7 +1,7 @@
-/* 
+/*
  * Project myProject
  * Author: Your Name
- * Date: 
+ * Date:
  * For comprehensive documentation and examples, please visit:
  * https://docs.particle.io/firmware/best-practices/firmware-template/
  */
@@ -22,21 +22,78 @@ SerialLogHandler logHandler(LOG_LEVEL_INFO);
 int MICROPHONE_PIN = A0;
 float val;
 
+/*SS => A5 (D14) (but can use any available GPIO)
+SCK => SCK (D13)
+MISO => MISO (D11)
+MOSI => MOSI (D12)*/
+
+/*he user's code must control the slave-select pin with digitalWrite() before and after each SPI transfer for the desired SPI slave device. Calling SPI.end() does NOT reset the pin mode of the SPI pins.*/
+int ss = A5;
+
 // setup() runs once, when the device is first turned on
-void setup() {
-  // Put initialization like pinMode and begin functions here
-  
+void setup()
+{
+  SPI.setBitOrder(order); // LSBFIRST (least-significant bit first) or MSBFIRST
+
+  // SPI.setClockSpeed(value, scale);
+  // SPI.setClockSpeed(frequency); //don't change system clock change clock divider?
+  /*
+  SPI_CLOCK_DIV2
+  SPI_CLOCK_DIV4
+  SPI_CLOCK_DIV8
+  SPI_CLOCK_DIV16
+  SPI_CLOCK_DIV32
+  SPI_CLOCK_DIV64
+  SPI_CLOCK_DIV128
+  SPI_CLOCK_DIV256
+  */
+  SPI.setClockDivider(divider); //clock reference is 64 MHz.
+
+  SPI.setDataMode(mode);
+
+  // spi setup https://docs.particle.io/reference/device-os/api/spi/
+  SPI.begin(SPI_MODE_MASTER, ss);
+
+
+
+  SPI.transfer(); // 0x10002, 0x3000A, 0x50008 (setup crossbars)
 }
 
-// loop() runs over and over again, as quickly as it can execute.
-void loop() {
-  // The core of your code will likely live here.
+void myFunction(uint8_t state) {
+  // called when selected or deselected
+  transfer();//transfer data to chip
+} //this function is called when chip is selected
 
-  // Example: Publish event to cloud every 10 seconds. Uncomment the next 3 lines to try it!
-  // Log.info("Sending Hello World to the cloud!");
-  // Particle.publish("Hello world!");
-  // delay( 10 * 1000 ); // milliseconds and blocking - see docs for more info!
+// loop() runs over and over again, as quickly as it can execute.
+void loop()
+{
+
   val = analogRead(MICROPHONE_PIN);
+  
+  SPI.onSelect(myFunction);//select chip
+
+  
+  
 
   Serial.print(val);
+
+
+
+  //example code 
+  /*
+  digitalWrite(chipSelectPin, LOW);  // Select the slave device
+
+    SPI.beginTransaction(spiSettings);  // Start the transaction
+
+    byte data = SPI.transfer(0x55);  // Send and receive a byte
+
+    Serial.print("Received data: ");
+    Serial.println(data, HEX);  // Print the received byte in hexadecimal
+
+    SPI.endTransaction();  // End the transaction
+
+    digitalWrite(chipSelectPin, HIGH);  // Deselect the slave device
+
+    delay(1000);  // Wait 1 second before sending again
+  */
 }
