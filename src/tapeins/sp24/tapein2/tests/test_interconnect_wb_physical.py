d1922fed22f326a773ae6607aa4cec43189624e1
c2s2-workstations <cornellcustomsiliconsystems@gmail.com>
2025-02-22 15:56:03 -0500

    Added tests for wb loopbacks, debugged test generation functions

--- src/tapeins/sp24/tapein2/tests/test_interconnect_wb_physical.py
+++ src/tapeins/sp24/tapein2/tests/test_interconnect_wb_physical.py
@@ -34,6 +34,10 @@ Test 5: output xbar    -> read2 -> write0 -> input xbar
 Test 6: output xbar    -> read2 -> write1 -> classifer xbar
 """
 
+# ------------------------------------------------------------------------
+# Message generation for loopback tests
+# ------------------------------------------------------------------------
+
 # Generates input/output msgs for spi -> inXbar -> wb -> clsXbar -> spi
 def wb_loopback_inXbar_clsXbar_msg(msgs):
     for x in msgs:
@@ -44,13 +48,15 @@ def wb_loopback_inXbar_clsXbar_msg(msgs):
         ] + msgs, msgs
 
 # Generates input/output msgs for spi -> inXbar -> wb -> outXbar -> spi
+# Because output xbar only takes the LSB of the wishbone output,
+# the expected message from xbar is the LSB of the input message (msgs & 1)
 def wb_loopback_inXbar_outXbar_msg(msgs):
     for x in msgs:
         assert x >= 0 and x < 0x10000
     return [
         input_xbar_config_msg(InXbarCfg.SPI_WSB),
         output_xbar_config_msg(OutXbarCfg.WSB_SPI)
-        ] + msgs, msgs
+        ] + msgs, msgs & 1
 
 # Generates input/output msgs for spi -> clsXbar -> wb -> inXbar -> spi
 def wb_loopback_clsXbar_inXbar_msg(msgs):
@@ -62,6 +68,7 @@ def wb_loopback_clsXbar_inXbar_msg(msgs):
         ] + msgs, msgs
 
 # Generates input/output msgs for spi -> clsXbar -> wb -> outXbar -> spi
+# The expected message from xbar is the LSB of the input message (msgs & 1)
 def wb_loopback_clsXbar_outXbar_msg(msgs):
     for x in msgs:
         assert x >= 0 and x < 0x10000
@@ -73,7 +80,7 @@ def wb_loopback_clsXbar_outXbar_msg(msgs):
 # Generates input/output msgs for spi -> outXbar -> wb -> inXbar -> spi
 def wb_loopback_outXbar_inXbar_msg(msgs):
     for x in msgs:
-        assert x >= 0 and x < 0x10000
+        assert x >= 0 and x < 0x10
     return [
         output_xbar_config_msg(OutXbarCfg.SPI_WSB),
         input_xbar_config_msg(InXbarCfg.WSB_SPI)
@@ -82,8 +89,108 @@ def wb_loopback_outXbar_inXbar_msg(msgs):
 # Generates input/output msgs for spi -> outXbar -> wb -> clsXbar -> spi
 def wb_loopback_outXbar_clsXbar_msg(msgs):
     for x in msgs:
-        assert x >= 0 and x < 0x10000
+        assert x >= 0 and x < 0x10
     return [
         output_xbar_config_msg(OutXbarCfg.SPI_WSB),
         cls_xbar_config_msg(ClsXbarCfg.WSB_SPI)
         ] + msgs, msgs
+
+# ------------------------------------------------------------------------
+# Loopback tests
+# ------------------------------------------------------------------------
+
+@pytest.mark.parametrize(
+    "msgs",
+    [
+        [0xFFFF],
+        [0x0000],
+        [0x5555, 0xAAAA],
+        [0xAAAA, 0x5555],
+        [0xDEAD, 0xBEEF, 0xCAFE, 0xBABE],
+        [0xABCD, 0x1234, 0x5678, 0x9ABC, 0xDEF0],
+    ],
+)
+def wb_test_loopback_inXbar_clsXbar(msgs, cmdline_opts):
+    in_msgs, out_msgs = wb_loopback_inXbar_clsXbar_msg(msgs)
+    dut = make_interconnect(cmdline_opts)
+    run_interconnect(dut, in_msgs, out_msgs)
+
+@pytest.mark.parametrize(
+    "msgs",
+    [
+        [0xFFFF],
+        [0x0000],
+        [0x5555, 0xAAAA],
+        [0xAAAA, 0x5555],
+        [0xDEAD, 0xBEEF, 0xCAFE, 0xBABE],
+        [0xABCD, 0x1234, 0x5678, 0x9ABC, 0xDEF0],
+    ],
+)
+def wb_test_loopback_inXbar_outXbar(msgs, cmdline_opts):
+    in_msgs, out_msgs = wb_loopback_inXbar_outXbar_msg(msgs)
+    dut = make_interconnect(cmdline_opts)
+    run_interconnect(dut, in_msgs, out_msgs)
+
+@pytest.mark.parametrize(
+    "msgs",
+    [
+        [0xFFFF],
+        [0x0000],
+        [0x5555, 0xAAAA],
+        [0xAAAA, 0x5555],
+        [0xDEAD, 0xBEEF, 0xCAFE, 0xBABE],
+        [0xABCD, 0x1234, 0x5678, 0x9ABC, 0xDEF0],
+    ],
+)
+def wb_test_loopback_clsXbar_inXbar(msgs, cmdline_opts):
+    in_msgs, out_msgs = wb_loopback_clsXbar_inXbar_msg(msgs)
+    dut = make_interconnect(cmdline_opts)
+    run_interconnect(dut, in_msgs, out_msgs)
+
+@pytest.mark.parametrize(
+    "msgs",
+    [
+        [0xFFFF],
+        [0x0000],
+        [0x5555, 0xAAAA],
+        [0xAAAA, 0x5555],
+        [0xDEAD, 0xBEEF, 0xCAFE, 0xBABE],
+        [0xABCD, 0x1234, 0x5678, 0x9ABC, 0xDEF0],
+    ],
+)
+def wb_test_loopback_clsXbar_outXbar(msgs, cmdline_opts):
+    in_msgs, out_msgs = wb_loopback_clsXbar_outXbar_msg(msgs)
+    dut = make_interconnect(cmdline_opts)
+    run_interconnect(dut, in_msgs, out_msgs)
+
+@pytest.mark.parametrize(
+    "msgs",
+    [
+        [0x1],
+        [0x0],
+        [0x1, 0x0],
+        [0x0, 0x1],
+        [0x1, 0x0, 0x1, 0x0],
+        [0x0, 0x1, 0x0, 0x1],
+    ],
+)
+def wb_test_loopback_outXbar_inXbar(msgs, cmdline_opts):
+    in_msgs, out_msgs = wb_loopback_outXbar_inXbar_msg(msgs)
+    dut = make_interconnect(cmdline_opts)
+    run_interconnect(dut, in_msgs, out_msgs)
+
+@pytest.mark.parametrize(
+    "msgs",
+    [
+        [0x1],
+        [0x0],
+        [0x1, 0x0],
+        [0x0, 0x1],
+        [0x1, 0x0, 0x1, 0x0],
+        [0x0, 0x1, 0x0, 0x1],
+    ],
+)
+def wb_test_loopback_outXbar_clsXbar(msgs, cmdline_opts):
+    in_msgs, out_msgs = wb_loopback_outXbar_clsXbar_msg(msgs)
+    dut = make_interconnect(cmdline_opts)
+    run_interconnect(dut, in_msgs, out_msgs)
