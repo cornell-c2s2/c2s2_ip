b14a4f82fe400a18c2d86d78e17f4e8c70a23e06
js2992 <js2992@cornell.edu>
2025-03-12 01:09:53 -0400

    FIFO in progress

--- src/systolic/FIFO.v
+++ src/systolic/FIFO.v
@@ -10,50 +10,41 @@ module FIFO
   input  logic             rst,
   input  logic             wen,
   input  logic             ren,
-  input  logic [nbits-1:0] d_in,
+  //input  logic [nbits-1:0] d_in,
   output logic             full,
   output logic             empty,
-  output logic [nbits-1:0] q_out
+  //output logic [nbits-1:0] q_out
 );
 
-  logic [nbits-1:0] q [size];
-
-  // R/W Pointers
+  // Status Signals
 
   logic _full;
   logic _empty;
-
-  logic [$clog2(size)-1:0] w_ptr;
-  logic [$clog2(size)-1:0] r_ptr;
-
-  assign _full  = ((w_ptr + 1) == r_ptr);
-  assign _empty = (w_ptr == r_ptr);
-
-  // Write logic
+  logic [$clog2(size)-1:0] count;
 
   always_ff @(posedge clk) begin
-    if(rst)
-      w_ptr <= 0;
-    else if(wen & !_full) begin
-      q[w_ptr] <= d_in;
-      w_ptr    <= w_ptr + 1;
+    if(rst) begin
+      count  <= 0;
+      _full  <= 0;
+      _empty <= 0;
+    end
+    else begin
+      case(wen)
+        // read only
+        1'b0: begin
+          count  <= (count - ren);
+          _full  <= 0;
+          _empty <= ((count - ren) == 0);
+        end
+        // write only
+        1'b1: begin
+          count <= count + !ren;
+          _full <= ((count + !ren) == size);
+          _empty <= 0;
+        end
+      endcase
     end
   end
-
-  // Read Logic
-
-  always_ff @(posedge clk) begin
-    if(rst)
-      r_ptr <= 0;
-    else if(ren & !_empty) begin
-      q_out <= q[r_ptr];
-      r_ptr <= r_ptr + 1;
-    end
-    else
-      q_out <= 0;
-  end
-
-  // Status Signals
 
   assign full  = _full;
   assign empty = _empty;
