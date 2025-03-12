d4f10242849831830df2508da87f66e76e0eeca0
js2992 <js2992@cornell.edu>
2025-03-12 16:13:14 -0400

    .

--- src/systolic/FIFO.v
+++ src/systolic/FIFO.v
@@ -3,76 +3,54 @@
 
 module FIFO
 #(
-  parameter size  = 3,
-  parameter nbits = 16
+  parameter depth     = 3,
+  parameter nbits     = 16,
+  parameter ptr_width = $clog2(depth)
 )(
   input  logic             clk,
   input  logic             rst,
   input  logic             wen,
   input  logic             ren,
-  input  logic [nbits-1:0] d_in,
+  //input  logic [nbits-1:0] d_in,
   output logic             full,
   output logic             empty,
-  output logic [nbits-1:0] q_out
+  //output logic [nbits-1:0] q_out
 );
 
   logic _full;
   logic _empty;
 
-  logic [$clog2(size)-1:0] wptr;
-  logic [$clog2(size)-1:0] rptr;
+  logic [ptr_width-1:0] wptr;
+  logic [ptr_width-1:0] rptr;
 
-  logic [$clog2(size)-1:0] wptr_next;
-  logic [$clog2(size)-1:0] rptr_next;
+  logic [ptr_width-1:0] wptr_next;
+  logic [ptr_width-1:0] rptr_next;
 
-  logic [nbits-1:0] q [size];
+  //logic [nbits-1:0] q [depth];
 
-  // Write-Only Logic
+  // R/W Pointers
 
-  assign wptr_next = ((wptr + 1) == size ? 0 : wptr + 1);
-
-  always_ff @(posedge clk) begin
-    if(rst) begin
-      wptr  <= 0;
-      _full <= 0;
-    end
-    else if(wen & ~ren & ~_full) begin
-      q[wptr] <= d_in;
-      wptr    <= wptr_next;
-      _full   <= (wptr_next == rptr);
-    end
-    else begin
-      q[wptr] <= q[wptr];
-      wptr  <= wptr;
-      _full <= _full;
-    end
-  end
-
-  assign full = _full;
-
-  // Read-Only Logic
-
-  assign rptr_next = ((rptr + 1) == size ? 0 : rptr + 1);
+  assign wptr_next = wptr + (wen & ~_full);
+  assign rptr_next = rptr + (ren & ~_empty);
 
   always_ff @(posedge clk) begin
     if(rst) begin
-      rptr   <= 0;
-      _empty <= 1;
-    end
-    else if(~wen & ren & ~_empty) begin
-      q_out  <= q[rptr];
-      rptr   <= rptr_next;
-      _empty <= (wptr == rptr_next);
-    end
-    else begin
-      q_out  <= 0;
-      rptr   <= rptr;
-      _empty <= _empty;
+      wptr <= 0;
+      rptr <= 0;
+    end else begin
+      wptr <= (wptr_next == depth ? 0 : wptr_next);
+      rptr <= (rptr_next == depth ? 0 : rptr_next);
     end
   end
 
+  // Status Signals
+
+  assign _full  = (wptr_next > rptr);
+  assign _empty = (wptr == rptr);
+
+  assign full  = _full;
   assign empty = _empty;
-
+  
 endmodule
 
 `endif
