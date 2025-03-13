7145feea3fb0c233d2c98356c59299ade0002c73
js2992 <js2992@cornell.edu>
2025-03-12 23:13:22 -0400

    fifo

--- src/systolic/SystolicFIFO.v
+++ src/systolic/SystolicFIFO.v
@@ -1,11 +1,11 @@
-`ifndef SYSTOLICBUFFER_V
-`define SYSTOLICBUFFER_V
+`ifndef SYSTOLICFIFO_V
+`define SYSTOLICFIFO_V
 
-module SystolicBuffer
+module SystolicFIFO
 #(
-  parameter depth     = 3,
+  parameter depth     = 4,
   parameter nbits     = 16,
-  parameter ptr_width = $clog2(depth)
+  parameter ptr_width = $clog2(depth) + 1
 )(
   input  logic             clk,
   input  logic             rst,
@@ -20,24 +20,16 @@ module SystolicBuffer
   logic _full;
   logic _empty;
 
-  logic write_only;
-  logic read_only;
-  
   logic [ptr_width-1:0] w_ptr;
   logic [ptr_width-1:0] r_ptr;
 
   logic [ptr_width-1:0] w_ptr_next;
   logic [ptr_width-1:0] r_ptr_next;
 
-  logic [nbits-1:0] mem [depth];
-
   // R/W Pointers
 
-  assign write_only = (wen & ~ren & ~_full);
-  assign read_only  = (ren & ~wen & ~_empty);
-
-  assign w_ptr_next = w_ptr + {{(ptr_width-1){1'b0}}, write_only};
-  assign r_ptr_next = r_ptr + {{(ptr_width-1){1'b0}}, read_only};
+  assign w_ptr_next = w_ptr + (wen & ~_full);
+  assign r_ptr_next = r_ptr + (ren & ~_empty);
 
   always_ff @(posedge clk) begin
     if(rst) begin
@@ -49,25 +41,18 @@ module SystolicBuffer
     end
   end
 
+  assign _full  = ((w_ptr - r_ptr) == depth);
+  assign _empty = (w_ptr == r_ptr);
+
   // Write Logic
 
-  always_ff @(posedge clk) begin
-    if(rst) begin
-      for(int i = 0; i < depth; i++)
-        mem[i] <= 0;
-    end else begin
-      mem[w_ptr] <= (write_only ? d : mem[w_ptr]);
-    end
-  end
+  //
 
   // Read Logic
 
-  assign q = (read_only ? mem[r_ptr] : 0);
+  //
 
   // Status Signals
-
-  assign _full  = (w_ptr == depth);
-  assign _empty = (r_ptr == depth);
 
   assign full  = _full;
   assign empty = _empty;
