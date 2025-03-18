3404c227015c9d59b76b440bc8a79092b8a3e0ec
js2992 <js2992@cornell.edu>
2025-03-18 12:49:09 -0400

    systolic array with fifo rtl

--- src/systolic/SystolicDpath.v
+++ src/systolic/SystolicDpath.v
@@ -1,3 +1,4 @@
+`include "systolic/SyncFIFO.v"
 `include "systolic/SystolicPE.v"
 
 `ifndef SYSTOLICDPATH_V
@@ -5,56 +6,95 @@
 
 module SystolicDpath
 #(
-  parameter size = 3,
-  parameter n = 16,
-  parameter d = 8
+  parameter size  = 16,
+  parameter nbits = 16,
+  parameter dbits = 8
 )(
   input  logic                    clk,
   input  logic                    rst,
-  input  logic                    en,
-  input  logic [n-1:0]            t_w_in [size],
-  input  logic [n-1:0]            l_x_in [size],
+  input  logic [nbits-1:0]        t_w_in   [size],
+  input  logic [nbits-1:0]        l_x_in   [size],
+  input  logic                    fifo_wen [size],
+  input  logic                    fifo_ren [size],
+  input  logic                    mac_en,
   input  logic [$clog2(size)-1:0] out_rsel,
   input  logic [$clog2(size)-1:0] out_csel,
-  output logic [n-1:0]            b_s_out
+  output logic [nbits-1:0]        b_s_out
 );
 
-logic [n-1:0] w [size+1][size];
-logic [n-1:0] x [size][size+1];
-logic [n-1:0] s [size][size];
+  genvar i, j;
 
-genvar i, j;
+  // Interal Data Flow Wires
 
-generate
-  for (j = 0; j < size; j++) begin : g_first_row
-    assign w[0][j] = t_w_in[j];
-  end
-endgenerate
-generate
-  for (i = 0; i < size; i++) begin : g_first_col
-    assign x[i][0] = l_x_in[i];
-  end
-endgenerate
+  logic [nbits-1:0] w [size+1][size];
+  logic [nbits-1:0] x [size][size+1];
+  logic [nbits-1:0] s [size][size];
 
-generate
-  for (i = 0; i < size; i++) begin : g_row
-    for (j = 0; j < size; j++) begin : g_cols
-      SystolicPE #(n, d, 1) PE
+  // Tensor FIFO
+
+  logic tensor_fifo_full;
+  logic tensor_fifo_empty;
+
+  generate
+    for(i = 0; i < size; i++) begin : g_tensor_fifo
+      SyncFIFO #(size, nbits) TensorFIFO
       (
         .clk   (clk),
         .rst   (rst),
-        .en    (en),
-        .x_in  (x[i][j]),
-        .w_in  (w[i][j]),
-        .x_out (x[i][j+1]),
-        .w_out (w[i+1][j]),
-        .s_out (s[i][j])
+        .wen   (fifo_wen[i]),
+        .ren   (fifo_ren[i]),
+        .d     (l_x_in[i]),
+        .q     (x[i][0]),
+        .full  (tensor_fifo_full),
+        .empty (tensor_fifo_empty)
       );
     end
-  end
-endgenerate
+  endgenerate
 
-assign b_s_out = s[out_rsel][out_csel];
+  // Weight FIFO
+
+  logic weight_fifo_full;
+  logic weight_fifo_empty;
+
+  generate
+    for(j = 0; j < size; j++) begin : g_weight_fifo
+      SyncFIFO #(size, nbits) WeightFIFO
+      (
+        .clk   (clk),
+        .rst   (rst),
+        .wen   (fifo_wen[j]),
+        .ren   (fifo_ren[j]),
+        .d     (t_w_in[j]),
+        .q     (w[0][j]),
+        .full  (weight_fifo_full),
+        .empty (weight_fifo_empty)
+      );
+    end
+  endgenerate
+
+  // Systolic Array
+
+  generate
+    for (i = 0; i < size; i++) begin : g_pe_rows
+      for (j = 0; j < size; j++) begin : g_pe_cols
+        SystolicPE #(nbits, dbits, 1) PE
+        (
+          .clk   (clk),
+          .rst   (rst),
+          .en    (en),
+          .x_in  (x[i][j]),
+          .w_in  (w[i][j]),
+          .x_out (x[i][j+1]),
+          .w_out (w[i+1][j]),
+          .s_out (s[i][j])
+        );
+      end
+    end
+  endgenerate
+
+  // Output Logic
+
+  assign b_s_out = s[out_rsel][out_csel];
 
 endmodule
 
