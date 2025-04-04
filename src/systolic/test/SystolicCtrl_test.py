94cf300af818b5364c1d759c83723373c98ae938
js2992 <js2992@cornell.edu>
2025-04-03 21:22:35 -0400

    ctrl fsm seems fine

--- src/systolic/test/SystolicCtrl_test.py
+++ src/systolic/test/SystolicCtrl_test.py
@@ -16,7 +16,6 @@ OUT  = 0b100
 #===========================================================
 
 async def d2c(dut, x_send_val, w_send_val, x_fifo_full, x_fifo_empty, w_fifo_full, w_fifo_empty):
-  dut.rst.value = 0
   dut.x_send_val.value = x_send_val
   dut.w_send_val.value = w_send_val
   
@@ -81,19 +80,111 @@ async def test_case_1_directed_fsm(dut):
   await always_ff(dut, LOAD, [0,0,0,0], [0,0,0,0])
 
   # @ MAC: FIFOs are read-enabled one at a time
-  await d2c(dut, 0, 0, [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0])
+  await d2c(dut, 0, 0, [1,1,1,1], [0,0,0,0], [1,1,1,1], [0,0,0,0])
   await always_comb(dut, 1, 0, 0, [0,0,0,0], [0,0,0,0])
   await always_ff(dut, MAC, [1,0,0,0], [1,0,0,0])
 
-  await d2c(dut, 0, 0, [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0])
+  await d2c(dut, 0, 0, [0,1,1,1], [0,0,0,0], [0,1,1,1], [0,0,0,0])
   await always_comb(dut, 1, 0, 0, [0,0,0,0], [0,0,0,0])
   await always_ff(dut, MAC, [1,1,0,0], [1,1,0,0])
 
-  await d2c(dut, 0, 0, [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0])
+  await d2c(dut, 0, 0, [0,0,1,1], [0,0,0,0], [0,0,1,1], [0,0,0,0])
   await always_comb(dut, 1, 0, 0, [0,0,0,0], [0,0,0,0])
   await always_ff(dut, MAC, [1,1,1,0], [1,1,1,0])
 
+  await d2c(dut, 0, 0, [0,0,0,1], [0,0,0,0], [0,0,0,1], [0,0,0,0])
+  await always_comb(dut, 1, 0, 0, [0,0,0,0], [0,0,0,0])
+  await always_ff(dut, MAC, [1,1,1,1], [1,1,1,1])
+
+  # @ MAC: FIFOs start to empty
+  await d2c(dut, 0, 0, [0,0,0,0], [1,0,0,0], [0,0,0,0], [1,0,0,0])
+  await always_comb(dut, 1, 0, 0, [0,0,0,0], [0,0,0,0])
+  await always_ff(dut, MAC, [1,1,1,1], [1,1,1,1])
+
+  await d2c(dut, 0, 0, [0,0,0,0], [1,1,0,0], [0,0,0,0], [1,1,0,0])
+  await always_comb(dut, 1, 0, 0, [0,0,0,0], [0,0,0,0])
+  await always_ff(dut, MAC, [1,1,1,1], [1,1,1,1])
+
+  await d2c(dut, 0, 0, [0,0,0,0], [1,1,1,0], [0,0,0,0], [1,1,1,0])
+  await always_comb(dut, 1, 0, 0, [0,0,0,0], [0,0,0,0])
+  await always_ff(dut, MAC, [1,1,1,1], [1,1,1,1])
+
+  # @ MAC: All FIFOs are empty
+  await d2c(dut, 0, 0, [0,0,0,0], [1,1,1,1], [0,0,0,0], [1,1,1,1])
+  await always_comb(dut, 1, 0, 0, [0,0,0,0], [0,0,0,0])
+  await always_ff(dut, MAC, [1,1,1,1], [1,1,1,1])
+
+  # @ OUT
+  await d2c(dut, 0, 0, [0,0,0,0], [1,1,1,1], [0,0,0,0], [1,1,1,1])
+  await always_comb(dut, 1, 0, 0, [0,0,0,0], [0,0,0,0])
+  await always_ff(dut, OUT, [0,0,0,0], [0,0,0,0])
+
+  await d2c(dut, 0, 0, [0,0,0,0], [1,1,1,1], [0,0,0,0], [1,1,1,1])
+  await always_comb(dut, 1, 0, 0, [0,0,0,0], [0,0,0,0])
+  await always_ff(dut, OUT, [0,0,0,0], [0,0,0,0])
+
+  await d2c(dut, 0, 0, [0,0,0,0], [1,1,1,1], [0,0,0,0], [1,1,1,1])
+  await always_comb(dut, 1, 0, 0, [0,0,0,0], [0,0,0,0])
+  await always_ff(dut, OUT, [0,0,0,0], [0,0,0,0])
+
+@cocotb.test()
+async def test_case_2_directed_fsm(dut):
+  cocotb.start_soon(Clock(dut.clk, 1, units="ns").start(start_high=False))
+
+  await reset(dut)
+
+  # @ LOAD: FIFOs are initially empty
+  await d2c(dut, 0, 0, [0,0,0,0], [1,1,1,1], [0,0,0,0], [1,1,1,1])
+  await always_comb(dut, 0, 0, 0, [0,0,0,0], [0,0,0,0])
+  await always_ff(dut, LOAD, [0,0,0,0], [0,0,0,0])
+
+  # @ LOAD: Tensor deserializer is full (write FIFOs)
+  await d2c(dut, 1, 0, [0,0,0,0], [1,1,1,1], [0,0,0,0], [1,1,1,1])
+  await always_comb(dut, 0, 1, 0, [1,1,1,1], [0,0,0,0])
+  await always_ff(dut, LOAD, [0,0,0,0], [0,0,0,0])
+
+  # @ LOAD: Weight deserializer is full (write FIFOs)
+  await d2c(dut, 0, 1, [0,0,0,0], [0,0,0,0], [0,0,0,0], [1,1,1,1])
+  await always_comb(dut, 0, 0, 1, [0,0,0,0], [1,1,1,1])
+  await always_ff(dut, LOAD, [0,0,0,0], [0,0,0,0])
+
+  # @ LOAD: FIFOs are neither empty nor full
   await d2c(dut, 0, 0, [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0])
+  await always_comb(dut, 0, 0, 0, [0,0,0,0], [0,0,0,0])
+  await always_ff(dut, LOAD, [0,0,0,0], [0,0,0,0])
+
+  #
+  # ...
+  #
+
+  # @ LOAD: Tensor FIFOs are full
+  await d2c(dut, 0, 0, [1,1,1,1], [0,0,0,0], [0,0,0,0], [0,0,0,0])
+  await always_comb(dut, 0, 0, 0, [0,0,0,0], [0,0,0,0])
+  await always_ff(dut, LOAD, [0,0,0,0], [0,0,0,0])
+
+  #
+  # ...
+  #
+
+  # @ LOAD: Weight FIFOs are full
+  await d2c(dut, 0, 0, [1,1,1,1], [0,0,0,0], [1,1,1,1], [0,0,0,0])
+  await always_comb(dut, 0, 0, 0, [0,0,0,0], [0,0,0,0])
+  await always_ff(dut, LOAD, [0,0,0,0], [0,0,0,0])
+
+  # @ MAC: FIFOs are read-enabled one at a time
+  await d2c(dut, 0, 0, [1,1,1,1], [0,0,0,0], [1,1,1,1], [0,0,0,0])
+  await always_comb(dut, 1, 0, 0, [0,0,0,0], [0,0,0,0])
+  await always_ff(dut, MAC, [1,0,0,0], [1,0,0,0])
+
+  await d2c(dut, 0, 0, [0,1,1,1], [0,0,0,0], [0,1,1,1], [0,0,0,0])
+  await always_comb(dut, 1, 0, 0, [0,0,0,0], [0,0,0,0])
+  await always_ff(dut, MAC, [1,1,0,0], [1,1,0,0])
+
+  await d2c(dut, 0, 0, [0,0,1,1], [0,0,0,0], [0,0,1,1], [0,0,0,0])
+  await always_comb(dut, 1, 0, 0, [0,0,0,0], [0,0,0,0])
+  await always_ff(dut, MAC, [1,1,1,0], [1,1,1,0])
+
+  await d2c(dut, 0, 0, [0,0,0,1], [0,0,0,0], [0,0,0,1], [0,0,0,0])
   await always_comb(dut, 1, 0, 0, [0,0,0,0], [0,0,0,0])
   await always_ff(dut, MAC, [1,1,1,1], [1,1,1,1])
 
