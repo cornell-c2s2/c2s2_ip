49c3962e69811b71e64cec471329762aa7d7cde2
Arjun Saini <69866165+Arjun-Saini@users.noreply.github.com>
2025-04-20 13:44:50 -0400

    Full coverage tests

--- src/drop_unit/drop_unit.v
+++ src/drop_unit/drop_unit.v
@@ -6,38 +6,38 @@
 `define DROP_UNIT_V
 
 module drop_unit #(
-  parameter int n_bits = 8,
-  parameter int max_n_cycles = 10  // The number of cycles the unit waits for before sending a sample
+  parameter int N_BITS = 8,
+  parameter int MAX_N_CYCLES = 10  // The max number of cycles the unit waits for before sending a sample
 ) (
   // Clock and Reset Ports
   input  logic  clk,
   input  logic  reset,
   input  logic  enable,                // If 1, disable drop unit (send every req_msg to resp_msg). In this mode, the drop unit acts basically like a wire.
  
-  input  logic [n_bits-1:0]            req_msg,
-  input  logic                         req_val,
-  output logic                         req_rdy,
+  input  logic [N_BITS-1:0]                req_msg,
+  input  logic                             req_val,
+  output logic                             req_rdy,
  
-  output  logic [n_bits-1:0]           resp_msg,
-  output  logic                        resp_val,
-  input logic                          resp_rdy,
+  output logic [N_BITS-1:0]                resp_msg,
+  output logic                             resp_val,
+  input  logic                             resp_rdy,
  
-  input logic    [max_n_cycles-1:0]    cfg_drop_msg, // A counter value to denote the number of cycles between captured samples.
-  input logic                          cfg_drop_val,  // Val signal for above
-  output logic                         cfg_drop_rdy
+  input  logic [$clog2(MAX_N_CYCLES)-1:0]  cfg_drop_msg, // A counter value to denote the number of cycles between captured samples.
+  input  logic                             cfg_drop_val,  // Val signal for above
+  output logic                             cfg_drop_rdy
 );
 
   localparam SEND = 2'd0, DROP = 2'd1;
-  logic [1:0] state, next_state;
+  logic state, next_state;
   
-  logic [$clog2(max_n_cycles)-1:0] n_cycles;
-  logic [$clog2(max_n_cycles)-1:0] counter;
+  logic [$clog2(MAX_N_CYCLES)-1:0] n_cycles;
+  logic [$clog2(MAX_N_CYCLES)-1:0] counter;
 
   // manage state
   always_comb begin
     case (state)
       SEND: begin
-        if (enable == 0 & req_val == 1) begin
+        if (enable == 0 & req_val == 1 & req_rdy == 1) begin
           next_state = DROP;
         end else begin
           next_state = SEND;
@@ -60,15 +60,15 @@ module drop_unit #(
   always_comb begin
     case (state)
       SEND: begin
-        req_rdy = 1;
-        resp_msg = req_msg;
-        resp_val = req_val;
+        req_rdy      = resp_rdy;
+        resp_msg     = req_msg;
+        resp_val     = req_val;
         cfg_drop_rdy = 1;
       end
       DROP: begin
-        req_rdy = 1;
-        resp_msg = 0;
-        resp_val = 0;
+        req_rdy      = 1;
+        resp_msg     = 0;
+        resp_val     = 0;
         cfg_drop_rdy = 0;
       end
       default: begin
--- src/drop_unit/tests/test_drop_unit.py
+++ src/drop_unit/tests/test_drop_unit.py
@@ -37,8 +37,10 @@ async def drop_test(dut, NUM_BITS, NUM_LOOPS, DROP_RATE):
     for i in range(NUM_LOOPS):
         dut.req_val.value = 1
         dut.req_msg.value = random.getrandbits(NUM_BITS)
+        dut.resp_rdy.value = 1
 
         await ClockCycles(dut.clk, 1)
+        dut.cfg_drop_val.value = 0
 
         assert dut.resp_val.value == 1
         assert dut.resp_msg.value == dut.req_msg.value
@@ -46,6 +48,7 @@ async def drop_test(dut, NUM_BITS, NUM_LOOPS, DROP_RATE):
         for j in range(DROP_RATE-1):
             dut.req_val.value = 1
             dut.req_msg.value = random.getrandbits(NUM_BITS)
+            dut.resp_rdy.value = 0
 
             await ClockCycles(dut.clk, 1)
             
@@ -60,7 +63,7 @@ MAX_N_CYCLES = 10
 random.seed(2)
 
 @cocotb.test()
-async def run_enable_test(dut):
+async def run_enable_test_1(dut):
     cocotb.start_soon(Clock(dut.clk, 1, "ns").start())
     for i in range(100):
         await enable_test(dut, N_BITS, random.randint(1,25), random.randint(2, MAX_N_CYCLES))
@@ -70,3 +73,9 @@ async def run_drop_test(dut):
     cocotb.start_soon(Clock(dut.clk, 1, "ns").start())
     for i in range(100):
         await drop_test(dut, N_BITS, random.randint(1, 25), random.randint(2, MAX_N_CYCLES))
+
+@cocotb.test()
+async def run_enable_test_2(dut):
+    cocotb.start_soon(Clock(dut.clk, 1, "ns").start())
+    for i in range(100):
+        await enable_test(dut, N_BITS, random.randint(1,25), random.randint(2, MAX_N_CYCLES))
