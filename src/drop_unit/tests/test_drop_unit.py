3ef018f8a9f50f2d4e7ea6328db7bbaa43a42171
Arjun Saini <69866165+Arjun-Saini@users.noreply.github.com>
2025-04-20 12:53:26 -0400

    Fix tests

--- src/drop_unit/tests/test_drop_unit.py
+++ src/drop_unit/tests/test_drop_unit.py
@@ -4,50 +4,23 @@ import cocotb
 from cocotb.triggers import Timer, Edge, RisingEdge, FallingEdge, ClockCycles
 from cocotb.clock import Clock
 
-'''
-@cocotb.test()
-async def enable_basic_test_1(dut):
-    cocotb.start_soon(Clock(dut.clk, 1, "ns").start())
-
+async def enable_test(dut, NUM_BITS, NUM_LOOPS, DROP_RATE):
+    await ClockCycles(dut.clk, 1)
     dut.reset.value = 1
     await ClockCycles(dut.clk, 1)
     dut.reset.value = 0
-    await ClockCycles(dut.clk, 1)
     
     dut.enable.value = 1
-    
-    dut.cfg_drop_val.value = 1
-    dut.cfg_drop_msg.value = 10
-    
-    for i in range(5):
-        for j in range(9):
-            dut.req_val.value = 1
-            dut.req_msg.value = j+1
-            assert(dut.resp_msg.value == dut.req_msg.value)
-            await ClockCycles(dut.clk, 1)
-        dut.req_val.value = 1
-        dut.req_msg.value = 10
-        assert(dut.resp_msg.value == dut.req_msg.value)
-        await ClockCycles(dut.clk, 1)
-'''
-async def enable_basic_test_2(dut, NUM_BITS, NUM_LOOPS, DROP_RATE):
-    dut.reset.value = 1
-    await ClockCycles(dut.clk, 1)
-    dut.reset.value = 0
-    await ClockCycles(dut.clk, 1)
-    
-    dut.enable.value = 1
-    
-    #dut.cfg_drop_val.value = 1
-    #dut.cfg_drop_msg.value = DROP_RATE
     
     for i in range(NUM_LOOPS):
         for j in range(DROP_RATE):
             dut.req_val.value = 1
             dut.req_msg.value = random.getrandbits(NUM_BITS)
-            assert(dut.resp_msg.value == dut.req_msg.value)
-            assert(dut.resp_val.value == 1)
+
             await ClockCycles(dut.clk, 1)
+
+            assert dut.resp_msg.value == dut.req_msg.value
+            assert dut.resp_val.value == 1
         
         
 
@@ -56,39 +29,44 @@ async def drop_test(dut, NUM_BITS, NUM_LOOPS, DROP_RATE):
     dut.reset.value = 1
     await ClockCycles(dut.clk, 1)
     dut.reset.value = 0
-    
-    
+
     dut.enable.value = 0
-    
     dut.cfg_drop_val.value = 1
     dut.cfg_drop_msg.value = DROP_RATE
-    await ClockCycles(dut.clk, 1)
+
     for i in range(NUM_LOOPS):
         dut.req_val.value = 1
         dut.req_msg.value = random.getrandbits(NUM_BITS)
 
         await ClockCycles(dut.clk, 1)
-        assert dut.resp_val.value == 1
 
+        assert dut.resp_val.value == 1
         assert dut.resp_msg.value == dut.req_msg.value
+
         for j in range(DROP_RATE-1):
             dut.req_val.value = 1
             dut.req_msg.value = random.getrandbits(NUM_BITS)
+
             await ClockCycles(dut.clk, 1)
+            
             assert dut.resp_val.value == 0
+
+    dut.req_val.value = 0
         
         
+N_BITS = 8
+MAX_N_CYCLES = 10
+
+random.seed(2)
 
 @cocotb.test()
 async def run_enable_test(dut):
     cocotb.start_soon(Clock(dut.clk, 1, "ns").start())
-    random.seed(2)
     for i in range(100):
-        await enable_basic_test_2(dut, 8, random.randint(1,25), random.randint(1,25))
+        await enable_test(dut, N_BITS, random.randint(1,25), random.randint(2, MAX_N_CYCLES))
 
 @cocotb.test()
 async def run_drop_test(dut):
     cocotb.start_soon(Clock(dut.clk, 1, "ns").start())
-    random.seed(2)
-    
-    await drop_test(dut, 8, random.randint(1,25), random.randint(1,25))
+    for i in range(100):
+        await drop_test(dut, N_BITS, random.randint(1, 25), random.randint(2, MAX_N_CYCLES))
