c583710674be27f1069410e2c7d6682641b377d3
Arjun Saini <69866165+Arjun-Saini@users.noreply.github.com>
2025-05-04 13:04:53 -0400

    collect outputs

--- src/drop_unit/sp25_demo/sp25_demo.py
+++ src/drop_unit/sp25_demo/sp25_demo.py
@@ -25,6 +25,13 @@ raw_bytes = raw_bytes[:len(raw_bytes)//(DROP_RATE * FFT_ARRAY_SIZE) * (DROP_RATE
 
 downsampled_bytes = drop_bytes(raw_bytes, DROP_RATE)
 
+fft_outputs = []
+    
+def int_to_twos_complement(value, num_bits=NUM_BITS):
+    if value < 0:
+        value = (1 << num_bits) + value
+    return format(value, f'0{num_bits}b')
+
 @cocotb.test()
 async def demo_check_drop(dut):
     cocotb.start_soon(Clock(dut.clk, 1, "ns").start())
@@ -49,20 +56,14 @@ async def demo_check_drop(dut):
             await ClockCycles(dut.clk, 1)
 
         v = BinaryValue(n_bits=NUM_BITS, binaryRepresentation=2)
-        v.signed_integer = raw_bytes[i]
-        if raw_bytes[i]:
-            dut.req_msg.value = v
-        else:
-            dut.req_msg.value = 0
+        v.binstr = int_to_twos_complement(raw_bytes[i])
+        dut.req_msg.value = v
         dut.req_val.value = 1
         await ClockCycles(dut.clk, 1)
         
         if dut.drop_unit.resp_val.value == 1:
             av = BinaryValue(n_bits=NUM_BITS, binaryRepresentation=2)
-            av.signed_integer = downsampled_bytes[i//DROP_RATE]
-            if downsampled_bytes[i//DROP_RATE]:
-                assert dut.drop_unit.resp_msg.value == av
-            else:
-                assert dut.drop_unit.resp_msg.value == 0
-        
-    await ClockCycles(dut.clk, FFT_ARRAY_SIZE)
+            av.binstr = int_to_twos_complement(downsampled_bytes[i//DROP_RATE])
+            assert dut.drop_unit.resp_msg.value == av
+        if dut.resp_val.value == 1:
+            fft_outputs.append(CFixed(v=(dut.resp_msg.value.integer, 0), n=NUM_BITS, d=8, raw=True))
