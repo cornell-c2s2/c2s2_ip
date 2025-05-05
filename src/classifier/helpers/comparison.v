3e1ce06462f1bdb7a82e667b3e64e7912551ce56
jjm469 <jjm469@cornell.edu>
2025-05-05 02:25:37 -0400

    Fixed bugs in classifier RTL. Now, we check if (freq of bin)>=cutoff_freq and (mag of bin) >= cutoff_mag. Also removed unnecessary decimal pt param.

--- src/classifier/helpers/comparison.v
+++ src/classifier/helpers/comparison.v
@@ -15,10 +15,16 @@ module comparison_Comparison #(
   output logic                   compare_out
 );
   logic [N_SAMPLES-1:0] compare_outs;
+  logic                 magnitude_val [N_SAMPLES];
+  
+  //Purely for probing in simulation. Not used otherwise...
+  logic                 bin_val [N_SAMPLES];          
 
   generate
     for (genvar i = 0; i < N_SAMPLES; i = i + 1) begin
-      assign compare_outs[i] = filtered_valid[i] & (mag_in[i] > cutoff_mag);
+      assign magnitude_val[i] = mag_in[i] >= cutoff_mag;
+      assign compare_outs[i] = filtered_valid[i] & magnitude_val[i];
+      assign bin_val[i] = filtered_valid[i] & magnitude_val[i];
     end
   endgenerate
 
