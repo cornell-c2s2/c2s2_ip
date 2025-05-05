fde8b8da1c9b22495fcf8e3970b1973044f60293
Arjun Saini <69866165+Arjun-Saini@users.noreply.github.com>
2025-05-05 02:09:43 -0400

    enable + update images

--- src/drop_unit/sp25_demo/sp25_demo.py
+++ src/drop_unit/sp25_demo/sp25_demo.py
@@ -22,15 +22,20 @@ import numpy as np
 
 # parameters
 NUM_BITS = 16
-FFT_ARRAY_SIZE = 32
+DECIMAL_BITS = 8
+FFT_ARRAY_SIZE = 64
 
 DROP_RATE = 10
+ENABLE = 0
 
 raw_bytes = wav_to_q8_8_messages("input.wav", 0)
-raw_bytes = raw_bytes[:len(raw_bytes)//(DROP_RATE * FFT_ARRAY_SIZE) * (DROP_RATE * FFT_ARRAY_SIZE) - 1]
+raw_bytes = raw_bytes[:len(raw_bytes)//(DROP_RATE * FFT_ARRAY_SIZE * 8) * (DROP_RATE * FFT_ARRAY_SIZE) - 1]
 
 dropped_bytes = drop_bytes(raw_bytes, DROP_RATE)
-# resampled_bytes = resample(raw_bytes, len(raw_bytes)//DROP_RATE)
+if ENABLE == 0:
+    resampled_bytes = resample(raw_bytes, len(raw_bytes)//DROP_RATE)
+else:
+    resampled_bytes = resample(raw_bytes, len(raw_bytes))
 
 fft_outputs = []
 
@@ -42,14 +47,18 @@ def int_to_twos_complement(value, num_bits=NUM_BITS):
 
 def analyze_real_fft_output(fft_out: list, is_external: int):
     n = len(fft_out)
+    if ENABLE == 0:
+        d = 1/(96000/DROP_RATE)
+    else:
+        d = 1/96000
 
-    freqs = np.fft.fftfreq(n, d=1/(96000/DROP_RATE))
+    freqs = np.fft.fftfreq(n, d=d)
     magnitudes = []
     
     for c in fft_out:
-        real = c.real.get(True) / (1 << 8)
-        imag = c.imag.get(True) / (1 << 8)
-        mag = (real**2 + imag**2)**0.5
+        real = c.real.get(True) / (1 << DECIMAL_BITS)
+        # imag = c.imag.get(True) / (1 << 8)
+        # mag = (real**2 + imag**2)**0.5
         magnitudes.append(real)
 
     plt.figure(figsize=(10, 5))
@@ -58,10 +67,16 @@ def analyze_real_fft_output(fft_out: list, is_external: int):
     plt.xlabel("Frequency (Hz)")
     plt.ylabel("Magnitude")
     plt.grid(True)
-    if(is_external == 1):
-        plt.savefig(f"figures/external_bins{DROP_RATE}.png")
+    if ENABLE == 0:
+        if(is_external == 1):
+            plt.savefig(f"figures/external_bins{DROP_RATE}.png")
+        else:
+            plt.savefig(f"figures/bins{DROP_RATE}.png")
     else:
-        plt.savefig(f"figures/bins{DROP_RATE}.png")
+        if(is_external == 1):
+            plt.savefig(f"figures/bins_external_enable.png")
+        else:
+            plt.savefig(f"figures/bins_enable.png")
 
 
 @cocotb.test()
@@ -70,7 +85,7 @@ async def demo_check_drop(dut):
     await ClockCycles(dut.clk, 1)
 
     dut.reset.value = 1
-    dut.enable.value = 0
+    dut.enable.value = ENABLE
     dut.enable_val.value = 1
     await ClockCycles(dut.clk, 1)
 
@@ -95,14 +110,17 @@ async def demo_check_drop(dut):
         
         if dut.drop_unit.resp_val.value == 1:
             av = BinaryValue(n_bits=NUM_BITS, binaryRepresentation=2)
-            av.binstr = int_to_twos_complement(dropped_bytes[i//DROP_RATE])
+            if ENABLE == 0:
+                av.binstr = int_to_twos_complement(dropped_bytes[i//DROP_RATE])
+            else:
+                av.binstr = int_to_twos_complement(raw_bytes[i])
             assert dut.drop_unit.resp_msg.value == av
         if dut.resp_val.value == 1:
-            fft_outputs.append(CFixed(v=(dut.resp_msg.value.integer, 0), n=NUM_BITS, d=8, raw=True))
+            fft_outputs.append(CFixed(v=(dut.resp_msg.value.integer, 0), n=NUM_BITS, d=DECIMAL_BITS, raw=True))
 
-    analyze_real_fft_output(fft_outputs[0:16], 0)
+    analyze_real_fft_output(fft_outputs[0:FFT_ARRAY_SIZE//2], 0)
 
-# model = FFTPease(16, 8, 32)
-# golden = model.transform([CFixed(v=(s, 0), n=16, d=8) for s in resampled_bytes[:32]])
-# fixed_outputs = [Fixed(x.real, True, 16, 8) for x in golden]
-# analyze_real_fft_output(golden[0:16], 1)
+    model = FFTPease(NUM_BITS, DECIMAL_BITS, FFT_ARRAY_SIZE)
+    golden = model.transform([CFixed(v=(s, 0), n=NUM_BITS, d=DECIMAL_BITS) for s in resampled_bytes[:FFT_ARRAY_SIZE]])
+    analyze_real_fft_output(golden[0:FFT_ARRAY_SIZE//2], 1)
+    
